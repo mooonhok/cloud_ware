0d10317547e5a861d91be3a410c0913d4bcc079d
mooonhok <1026413232@qq.com>
2017-08-16 14:10:37 +0800

    员工修改，和测试

--- stafftest.php
+++ stafftest.php
@@ -2,99 +2,73 @@
 /**
  * Created by PhpStorm.
  * User: Administrator
- * Date: 2017/7/21
- * Time: 10:10
+ * Date: 2017/8/16
+ * Time: 13:49
  */
 require 'Slim/Slim.php';
 require 'connect.php';
-use Slim\PDO\Database;
-
-
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
-$app->post('/staff',function()use($app){
+$app->post('/stafftest',function()use($app){
     $app->response->headers->set('Content-Type','application/json');
     $tenant_id=$app->request->headers->get('tenant-id');
     $database=localhost();
     $body=$app->request->getBody();
     $body=json_decode($body);
+    $staff_id=$body->staff_id;
     $username=$body->username;
+    $password=$body->password;
     $name=$body->name;
     $telephone=$body->telephone;
     $position=$body->position;
-    $staff_status=$body->staff_status;
+    $staff_status=$body->status;
     $permissions=$body->permissions;
     $array=array();
     foreach($body as $key=>$value){
         $array[$key]=$value;
     }
     if($tenant_id!=null||$tenant_id!=''){
-        $selectStatement = $database->select()
-            ->from('tenant')
-            ->where('exist',"=",0)
-            ->where('tenant_id','=',$tenant_id);
-        $stmt = $selectStatement->execute();
-         $data2 = $stmt->fetch();
-          if($data2!=null){
-              if($username!=null||$username!=''){
-                  if($name!=null||$name!=''){
-                      if($telephone!=null||$telephone!=''){
-                          if(preg_match("/^1[34578]\d{9}$/", $telephone)){
-                              $selectStatement = $database->select()
-                                  ->from('staff')
-                                  ->where('exist','=','0')
-                                  ->where('telephone','=',$telephone);
-                              $stmt = $selectStatement->execute();
-                              $data1 = $stmt->fetch();
-                              if($data1==null){
-                                  if($position!=null||$position!=''){
-                                      if($staff_status!=null||$staff_status!=''){
-                                          if($permissions!=null||$permissions!=''){
-                                              $selectStatement = $database->select()
-                                                  ->from('staff')
-                                                  ->where('tenant_id','=',$tenant_id);
-                                              $stmt = $selectStatement->execute();
-                                              $data = $stmt->fetchAll();
-                                              if($data!=null){
-                                                  $staff_id=count($data)+1001;
-                                              }else{
-                                                  $staff_id=1001;
-                                              }
-                                              $array['tenant_id']=$tenant_id;
-                                              $array['staff_id']=$staff_id;
-                                              $array['exist']=0;
-                                              $insertStatement = $database->insert(array_keys($array))
-                                                  ->into('staff')
-                                                  ->values(array_values($array));
-                                              $insertId = $insertStatement->execute(false);
-                                              echo json_encode(array("result"=>"0","desc"=>"success"));
-                                          }else{
-                                              echo json_encode(array('result'=>'1','desc'=>'缺少权限信息'));
-                                          }
-                                      }else{
-                                          echo json_encode(array('result'=>'2','desc'=>'缺少状态（在职，离职，实习）'));
-                                      }
-                                  }else{
-                                      echo json_encode(array('result'=>'3','desc'=>'缺少职位'));
-                                  }
-                              }else{
-                                  echo json_encode(array('result'=>'9','desc'=>'该电话已经存在'));
-                              }
-                          }else{
-                              echo json_encode(array('result'=>'4','desc'=>'电话号码不符合要求'));
-                          }
-                      }else{
-                          echo json_encode(array('result'=>'5','desc'=>'缺少电话'));
-                      }
-                  }else{
-                      echo json_encode(array('result'=>'6','desc'=>'缺少姓名'));
-                  }
-              }else{
-                  echo json_encode(array('result'=>'7','desc'=>'缺少用户名'));
-              }
-          }else{
-              echo json_encode(array('result'=>'10','desc'=>'该租户不存在'));
-          }
+        if($username!=null||$username!=''){
+            if($name!=null||$name!=''){
+                if($telephone!=null||$telephone!=''){
+                        if($position!=null||$position!=''){
+                            if($staff_status!=null||$staff_status!=''){
+                                if($permissions!=null||$permissions!=''){
+                                    if($staff_id!=null||$staff_id!=''){
+                                        if($password!=null||$password!=''){
+                                            $array['tenant_id']=$tenant_id;
+                                            $array['exist']=0;
+                                            $insertStatement = $database->insert(array_keys($array))
+                                                ->into('staff')
+                                                ->values(array_values($array));
+                                            $insertId = $insertStatement->execute(false);
+                                            echo json_encode(array("result"=>"0","desc"=>"success"));
+                                        }else{
+                                            echo json_encode(array('result'=>'1','desc'=>'缺少密码'));
+                                        }
+                                    }else{
+                                        echo json_encode(array('result'=>'1','desc'=>'缺少员工id'));
+                                    }
+
+
+                                }else{
+                                    echo json_encode(array('result'=>'1','desc'=>'缺少权限信息'));
+                                }
+                            }else{
+                                echo json_encode(array('result'=>'2','desc'=>'缺少状态（在职，离职，实习）'));
+                            }
+                        }else{
+                            echo json_encode(array('result'=>'3','desc'=>'缺少职位'));
+                        }
+                }else{
+                    echo json_encode(array('result'=>'5','desc'=>'缺少电话'));
+                }
+            }else{
+                echo json_encode(array('result'=>'6','desc'=>'缺少姓名'));
+            }
+        }else{
+            echo json_encode(array('result'=>'7','desc'=>'缺少用户名'));
+        }
     }else{
         echo json_encode(array('result'=>'8','desc'=>'缺少租户id'));
     }
@@ -112,13 +86,6 @@ $app->put('/staff',function()use($app){
         $array[$key]=$value;
     }
     if($tenant_id!=null||$tenant_id!=''){
-        $selectStatement = $database->select()
-            ->from('tenant')
-            ->where('exist',"=",0)
-            ->where('tenant_id','=',$tenant_id);
-        $stmt = $selectStatement->execute();
-        $data2 = $stmt->fetch();
-        if($data2!=null){
         if($staff_id!=null||$staff_id!=''){
             $selectStatement = $database->select()
                 ->from('staff')
@@ -138,11 +105,8 @@ $app->put('/staff',function()use($app){
             }else{
                 echo json_encode(array('result'=>'1','desc'=>'该员工不存在'));
             }
-            }else{
-             echo json_encode(array('result'=>'2','desc'=>'缺少员工id'));
-           }
         }else{
-            echo json_encode(array('result'=>'10','desc'=>'该租户不存在'));
+            echo json_encode(array('result'=>'2','desc'=>'缺少员工id'));
         }
     }else{
         echo json_encode(array('result'=>'3','desc'=>'缺少租户信息'));
@@ -162,13 +126,6 @@ $app->put('/staff',function()use($app){
         $array[$key]=$value;
     }
     if($tenant_id!=null||$tenant_id!=''){
-        $selectStatement = $database->select()
-            ->from('tenant')
-            ->where('exist',"=",0)
-            ->where('tenant_id','=',$tenant_id);
-        $stmt = $selectStatement->execute();
-        $data2 = $stmt->fetch();
-        if($data2!=null){
         if($staff_id!=null||$staff_id!=''){
             if($password!=null||$password!=''){
                 $selectStatement = $database->select()
@@ -195,9 +152,6 @@ $app->put('/staff',function()use($app){
         }else{
             echo json_encode(array('result'=>'3','desc'=>'缺少员工id'));
         }
-        }else{
-            echo json_encode(array('result'=>'10','desc'=>'该租户不存在'));
-        }
     }else{
         echo json_encode(array('result'=>'4','desc'=>'缺少租户id'));
     }
@@ -210,13 +164,6 @@ $app->get('/staff',function()use($app){
     $per_page=$app->request->get("per_page");
     $database=localhost();
     if($tenant_id!=null||$tenant_id!=""){
-        $selectStatement = $database->select()
-            ->from('tenant')
-            ->where('exist',"=",0)
-            ->where('tenant_id','=',$tenant_id);
-        $stmt = $selectStatement->execute();
-        $data2 = $stmt->fetch();
-        if($data2!=null){
         if($page==null||$per_page==null){
             $selectStatement = $database->select()
                 ->from('staff')
@@ -235,9 +182,6 @@ $app->get('/staff',function()use($app){
             $data = $stmt->fetchAll();
             echo json_encode(array("result"=>"0","desc"=>"success","staff"=>$data));
         }
-        }else{
-            echo json_encode(array('result'=>'10','desc'=>'该租户不存在'));
-        }
     }else{
         echo json_encode(array("result"=>"1","desc"=>"信息不全","staff"=>""));
     }
@@ -249,13 +193,6 @@ $app->delete('/staff',function()use($app){
     $database=localhost();
     $staff_id=$app->request->get('staffid');
     if ($tenant_id!=null||$tenant_id!=''){
-        $selectStatement = $database->select()
-            ->from('tenant')
-            ->where('exist',"=",0)
-            ->where('tenant_id','=',$tenant_id);
-        $stmt = $selectStatement->execute();
-        $data2 = $stmt->fetch();
-        if($data2!=null){
         if($staff_id!=null||$staff_id!=''){
             $selectStatement = $database->select()
                 ->from('staff')
@@ -277,9 +214,6 @@ $app->delete('/staff',function()use($app){
             }
         }else{
             echo json_encode(array("result"=>"2","desc"=>"缺少员工id"));
-        }
-        }else{
-            echo json_encode(array('result'=>'10','desc'=>'该租户不存在'));
         }
     }else{
         echo json_encode(array("result"=>"3","desc"=>"缺少租户id"));
