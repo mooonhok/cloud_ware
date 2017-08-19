2fc4c180385787aa171cabd71603e47485120f09
GHOSTDO <2121066061@qq.com>
2017-08-19 11:05:52 +0800

    微信测试2

--- test.php
+++ test.php
@@ -5,10 +5,50 @@
  * Date: 2017/8/18
  * Time: 17:12
  */
+require 'Slim/Slim.php';
+require 'connect.php';
+
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+
 $appid='wx15ef051f9f0bba92';
-$redirect_uri = urlencode ( 'http://mooonhok-cloudware.daoapp.io/test12.php' );
+$redirect_uri = urlencode ( 'http://mooonhok-cloudware.daoapp.io/test.php' );
 $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
 header("Location:".$url);
+function getopenid(){
+    $appid = "wx15ef051f9f0bba92";
+    $secret = "57ea0ee4abf4f4c6d6e38c88a289e687";
+    $code = $_GET["code"];
+
+//第一步:取全局access_token
+    $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$secret";
+    $token = getJson($url);
+
+//第二步:取得openid
+    $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+    $oauth2 = getJson($oauth2Url);
+    return $oauth2;
+}
 
 
+function getJson($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($output, true);
+}
+
+$app->post('/test',function()use($app){
+    echo getopenid();
+
+
+
+
+});
+$app->run();
 ?>
