c8d440f15161464709641aed42eaefa95bb07c71
GHOSTDO <2121066061@qq.com>
2017-08-19 11:33:34 +0800

    微信授权测试

--- test.php
+++ test.php
@@ -28,7 +28,13 @@ function getopenid(){
 //第二步:取得openid
     $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
     $oauth2 = getJson($oauth2Url);
-    return $oauth2;
+    //第三步:根据全局access_token和openid查询用户信息
+    $access_token = $token["access_token"];
+    $openid = $oauth2['openid'];
+    $get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
+    $userinfo = getJson($get_user_info_url);
+
+    return $userinfo;
 }
 
 
@@ -44,10 +50,10 @@ function getJson($url){
 }
 
 $app->post('/test',function()use($app){
+    $app->response->headers->set('Content-Type','application/json');
+    $body=$app->request->getBody();
+    $body=json_decode($body);
     echo getopenid();
-
-
-
 
 });
 $app->run();
