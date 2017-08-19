79ca211dd53220a569e1726d870fb448728172eb
GHOSTDO <2121066061@qq.com>
2017-08-19 15:05:07 +0800

    11

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 <?php
+require 'Slim/Slim.php';
+
+
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+$app->post('/test',function()use($app){
+    $sum=getBaseInfo();
+    echo $sum;
+});
+$app->run();
  function getBaseInfo(){
 	 
 	 $appid = "wx15ef051f9f0bba92";
@@ -6,6 +17,5 @@
 	 $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appid."&redirect_uri=".$redirect_uri."&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
 	 header('location:'.$url);
 }
-getBaseInfo();
 
 ?>
