4e9da3225bd9a1341b35b683d617a98c37599043
GHOSTDO <2121066061@qq.com>
2017-08-19 10:38:36 +0800

    微信测试

--- test.php
+++ test.php
@@ -10,9 +10,5 @@ $redirect_uri = urlencode ( 'http://mooonhok-cloudware.daoapp.io/test12.php' );
 $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
 header("Location:".$url);
 
-$app->run();
 
-function localhost(){
-    return connect();
-}
 ?>
