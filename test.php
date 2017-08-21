6c696177375fa971cc0c1def57118243b25640fc
GHOSTDO <2121066061@qq.com>
2017-08-21 09:05:58 +0800

    微信混搭

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
  function getBaseInfo(){
-	 
+
 	 $appid = "wx15ef051f9f0bba92";
 	 $redirect_uri = urlencode("http://mooonhok-cloudware.daoapp.io/test01.php");
 	 $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appid."&redirect_uri=".$redirect_uri."&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
--- test01.php
+++ test01.php
@@ -22,10 +22,6 @@ if(isset($_SESSION['openId'])){
     $_SESSION['openId'] = $openid;
 }
     echo '微信用户：'.$openid.'<br/>';
-    echo '$json_obj 返回信息：';
-    var_dump($json_obj);
-
-    getUserInfo($json_obj);
 /**
  * @param $json_obj
  * 根据用户openid 获取其所有信息
@@ -43,11 +39,128 @@ function getUserInfo($json_obj){
     $res = curl_exec($ch);
     curl_close($ch);
 
+
 //解析json
     $user_obj = json_decode($res,true);
     var_dump($user_obj);
-    echo 'Name:'.$user_obj['nickname'];
+    return 'Name:'.$user_obj['nickname'];
 }
 
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width,initial-scale=1.0,maximum-scale=1,minimum-scale=0.1,user-scalable=0" name="viewport">
+    <title></title>
+    <script type="text/javascript">
+        $(function(){
+            //改变div的高度
+            $(".content").css("height", $(window).height());
+        });
+    </script>
+    <style>
+        *{
+            margin:0 auto;
+            padding:0;
+            box-sizing:border-box;
+        }
+        .content{
+            background-color:#24D9C9;
+            width:100%;
+        }
+        .box_top{
+            width: 100%;
+            height:150px;
+            background-color:#24D9C9;
+        }
+        .box_top1{
+            width: 100%;
+            height: 60px;
+            padding-top: 80px;
+            font: 40px "微软雅黑";
+            text-align: center;
+            color:white;
+        }
+        .box_center{
+            border-radius:10px;
+            width: 90%;
+            height: 250px;
+            background-color:#2EBEB1;
+            font: 30px "微软雅黑";
+            text-align: center;
+            color:white;
+        }
+        .box_center1{
+            width: 100%;
+            height: 50px;
+            margin-top: 15px;
+            line-height:50px;
+        }
+        .box_center1 span{
+            height: 50px;
+            font: 18px "微软雅黑";
+            color:white;
+        }
+        .box_center1 input{
+            border-radius:50px;
+            width: 75%;
+            height:45px;
+            background-color:#42A199;
+            line-height: 45px;
+            border-style: none;
+            font: 15px "微软雅黑";
+            color:#FFFFFF;
+            padding:0 18px;
+            outline: 0;
+        }
+        .box_center1 input::-webkit-input-placeholder {
+            color:#9CD2CD;
+        }
+        .box_center1 input:-moz-placeholder {
+            color:#9CD2CD;
+            opacity:1;
+        }
+        .box_center1 input::-moz-placeholder {
+            color:#9CD2CD;
+            opacity:1;
+        }
+        .box_center1 input:-ms-input-placeholder {
+            color:#9CD2CD;
+        }
+        .box_buttom{
+            width: 100%;
+            height: 100px;
+            text-align: center;
+            margin-top: 20px;
+        }
+        #submit{
+            border-radius:50px;
+            width: 75%;
+            height:50px;
+            background-color:#005757;
+            margin-top: 22px;
+            font: 25px "微软雅黑";
+            line-height: 50px;
+            text-align: center;
+            color:#48D1CC;
+            background-color:white;
+        }
+    </style>
+</head>
+<body>
+<div class="content">
+    <div class="box_top"><div class="box_top1">Welcome</div></div>
+    <div class="box_center">
+        <div class="box_center1"><span>注册</span></div>
+        <div class="box_center1"><input id="customername" placeholder="货主名称"></input></div>
+        <div class="box_center1"><input id="customertel"  placeholder="手机号码"></input></div>
+    </div>
+    <div class="box_buttom">
+        <div id="submit">注册</div>
+    </div>
+</div>
+</body>
+
+</html>
+
