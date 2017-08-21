38b74e3cc56cf3a275e6f26c2a7f0cc75556dbbc
mooonhok <1026413232@qq.com>
2017-08-21 18:33:11 +0800

    1

--- test.php
+++ test.php
@@ -26,7 +26,7 @@ if(!isset($_GET['code'])){
     $secret = "57ea0ee4abf4f4c6d6e38c88a289e687";
     $code = $_GET["code"];
     $url1='http://mooonhok-cloudware.daoapp.io/weixin/chenggong.html';
-    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code&redirect_uri=$redirect_uri";
+    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code&redirect_uri=$url1";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
