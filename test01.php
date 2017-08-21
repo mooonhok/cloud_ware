f9d0625472bfa2ed8ad59a04b23020b1ead2761d
GHOSTDO <2121066061@qq.com>
2017-08-21 17:22:46 +0800

    微信cookie

--- test01.php
+++ test01.php
@@ -6,26 +6,25 @@ $secret = "57ea0ee4abf4f4c6d6e38c88a289e687";
 
 if(isset($_SESSION['openId'])){
     $openid = $_SESSION['openId'];
-}else{
+}else {
     $code = $_GET['code'];
-    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$secret."&code=".$code."&grant_type=authorization_code ";
+    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" . $appid . "&secret=" . $secret . "&code=" . $code . "&grant_type=authorization_code ";
 
     $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_HEADER,0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
     $res = curl_exec($ch);
     curl_close($ch);
-    $json_obj = json_decode($res,true);
+    $json_obj = json_decode($res, true);
     $openid = $json_obj['openid'];
     $_SESSION['openId'] = $openid;
-    if ($_COOKIE==null){
-       setcookie('openid',$openid);
+    if ($_COOKIE == null) {
+        setcookie('openid', $openid);
     }
 }
-$redirect = urlencode("http://mooonhok-cloudware.daoapp.io/weixin/zhuce.html");
-header('location:'.$redirect);
+
 //    echo '用户信息'.$openid.'<br/>';
 //    echo $_COOKIE['opendid'];
 function getUserInfo($json_obj){
@@ -45,5 +44,7 @@ function getUserInfo($json_obj){
     var_dump($user_obj);
     return 'Name:'.$user_obj['nickname'];
 }
+
+header('location:http://mooonhok-cloudware.daoapp.io/weixin/zhuce.html');
 ?>
 
