70d922ca81eb0975d137cd6068924ed58c514287
mooonhok <1026413232@qq.com>
2017-08-21 10:25:09 +0800

    微信测试

--- wx_interface.php
+++ wx_interface.php
@@ -36,6 +36,7 @@ class wechatCallbackapiTest
         if (!empty($postStr)){
 
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $_SESSION['openid']=$postObj->FromUserName;
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
             $keyword = trim($postObj->Content);
