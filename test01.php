311738bfa99b57e5db2e7892f41c48f291b66294
GHOSTDO <2121066061@qq.com>
2017-08-21 15:46:09 +0800

    微信三次跳授权测试

--- test01.php
+++ test01.php
@@ -21,12 +21,13 @@ if(isset($_SESSION['openId'])){
     $openid = $json_obj['openid'];
     $_SESSION['openId'] = $openid;
     if ($_COOKIE==null){
-       setcookie('opendid',$openid);
+       setcookie('openid',$openid);
     }
+    $redirect_uri = urlencode("http://mooonhok-cloudware.daoapp.io/weixin/zhuce.html");
+    header('location:'.$url);
 }
-
-    echo '用户信息'.$openid.'<br/>';
-    echo $_COOKIE['opendid'];
+  //  echo '用户信息'.$openid.'<br/>';
+    //echo $_COOKIE['opendid'];
 function getUserInfo($json_obj){
     $access_token = $json_obj['access_token'];
     $openid = $json_obj['openid'];
@@ -40,132 +41,9 @@ function getUserInfo($json_obj){
     $res = curl_exec($ch);
     curl_close($ch);
 
-
-//����json
     $user_obj = json_decode($res,true);
     var_dump($user_obj);
     return 'Name:'.$user_obj['nickname'];
 }
-echo '<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width,initial-scale=1.0,maximum-scale=1,minimum-scale=0.1,user-scalable=0" name="viewport">
-    <title></title>
-    <script type="text/javascript">
-        $(function(){
-           
-            $(".content").css("height", $(window).height());
-        });
-    </script>
-    <style>
-        *{
-            margin:0 auto;
-            padding:0;
-            box-sizing:border-box;
-        }
-        .content{
-            background-color:#24D9C9;
-            width:100%;
-        }
-        .box_top{
-            width: 100%;
-            height:150px;
-            background-color:#24D9C9;
-        }
-        .box_top1{
-            width: 100%;
-            height: 60px;
-            padding-top: 80px;
-            font: 40px "微软雅黑";
-            text-align: center;
-            color:white;
-        }
-        .box_center{
-            border-radius:10px;
-            width: 90%;
-            height: 250px;
-            background-color:#2EBEB1;
-            font: 30px "微软雅黑";
-            text-align: center;
-            color:white;
-        }
-        .box_center1{
-            width: 100%;
-            height: 50px;
-            margin-top: 15px;
-            line-height:50px;
-        }
-        .box_center1 span{
-            height: 50px;
-            font: 18px "微软雅黑";
-            color:white;
-        }
-        .box_center1 input{
-            border-radius:50px;
-            width: 75%;
-            height:45px;
-            background-color:#42A199;
-            line-height: 45px;
-            border-style: none;
-            font: 15px "微软雅黑";
-            color:#FFFFFF;
-            padding:0 18px;
-            outline: 0;
-        }
-        .box_center1 input::-webkit-input-placeholder {
-            color:#9CD2CD;
-        }
-        .box_center1 input:-moz-placeholder {
-            color:#9CD2CD;
-            opacity:1;
-        }
-        .box_center1 input::-moz-placeholder {
-            color:#9CD2CD;
-            opacity:1;
-        }
-        .box_center1 input:-ms-input-placeholder {
-            color:#9CD2CD;
-        }
-        .box_buttom{
-            width: 100%;
-            height: 100px;
-            text-align: center;
-            margin-top: 20px;
-        }
-        #submit{
-            border-radius:50px;
-            width: 75%;
-            height:50px;
-            background-color:#005757;
-            margin-top: 22px;
-            font: 25px "微软雅黑";
-            line-height: 50px;
-            text-align: center;
-            color:#48D1CC;
-            background-color:white;
-        }
-    </style>
-</head>
-<body>
-<div class="content">
-    <div class="box_top"><div class="box_top1">Welcome</div></div>
-    <div class="box_center">
-        <div class="box_center1"><span>注册</span></div>
-        <div class="box_center1"><input id="customername" placeholder="货主名称"></input></div>
-        <div class="box_center1"><input id="customertel"  placeholder="手机号"></input></div>
-    </div>
-    <div class="box_buttom">
-        <div id="submit">注册</div>
-    </div>
-</div>
-</body>
-
-</html>
-'
-
-
-
-
 ?>
 
