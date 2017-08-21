89c678168dbacfd841520a7cb8211c42f98b03f7
GHOSTDO <2121066061@qq.com>
2017-08-21 17:07:34 +0800

    微信测试

--- test03.php
+++ test03.php
@@ -8,9 +8,7 @@
 header('Content-type:text/html;charset=utf-8');
 if(!isset($_GET['code'])){
     $appid='wx15ef051f9f0bba92';
-    $redirect_uri = urlencode ( 'http://mooonhok-cloudware.daoapp.io/test03.php 
-
-' );
+    $redirect_uri = urlencode ( 'http://mooonhok-cloudware.daoapp.io/test03.php');
     $scope='snsapi_base';
     $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
     header('Location:'.$url);
@@ -18,9 +16,7 @@ if(!isset($_GET['code'])){
     $appid = "wx15ef051f9f0bba92";
     $secret = "57ea0ee4abf4f4c6d6e38c88a289e687";
     $code = $_GET["code"];
-    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$ 
-
-appid&secret=$secret&code=$code&grant_type=authorization_code";
+    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
@@ -28,7 +24,7 @@ appid&secret=$secret&code=$code&grant_type=authorization_code";
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     curl_close($ch);
-    $json_obj=json_decode($output, true);
+    $json_obj=json_decode($output,true);
     echo $json_obj['openid'];
 }
 ?>
