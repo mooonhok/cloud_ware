7ad70da91f4af8e5a46832ee7157a3cd786b3e60
GHOSTDO <2121066061@qq.com>
2017-08-21 17:05:33 +0800

    微信测试

--- test03.php
+++ test03.php
@@ -12,9 +12,7 @@ if(!isset($_GET['code'])){
 
 ' );
     $scope='snsapi_base';
-    $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$ 
-
-appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
+    $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
     header('Location:'.$url);
 }else{
     $appid = "wx15ef051f9f0bba92";
