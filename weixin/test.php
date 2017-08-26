2e04110dd90c62024eefa6e478710d53d0a43265
GHOSTDO <2121066061@qq.com>
2017-08-26 13:32:37 +0800

    扫描测试

---
+++ weixin/test.php
@@ -0,0 +1,63 @@
+<?php
+require_once "../test4.php";
+$jssdk = new JSSDK("wx15ef051f9f0bba92", "57ea0ee4abf4f4c6d6e38c88a289e687");
+$signPackage = $jssdk->GetSignPackage();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    	<style type="text/css">
+    	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <title></title>
+</head>
+<body>
+   <div id="saoman">扫码<div>
+</body>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+    /*
+     * 注意：
+     * 1. 所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
+     * 2. 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
+     * 3. 常见问题及完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
+     *
+     * 开发中遇到问题详见文档“附录5-常见错误及解决办法”解决，如仍未能解决可通过以下渠道反馈：
+     * 邮箱地址：weixin-open@qq.com
+     * 邮件主题：【微信JS-SDK反馈】具体问题
+     * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
+     */
+    wx.config({
+        debug: true,
+        appId: '<?php echo $signPackage["appId"];?>',
+        timestamp: <?php echo $signPackage["timestamp"];?>,
+        nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+        signature: '<?php echo $signPackage["signature"];?>',
+        jsApiList: [
+            'checkJsApi', 'scanQRCode'
+        ]
+    });
+    wx.ready(function () { 
+//// 9 微信原生接口  
+//// 9.1.1 扫描二维码并返回结果  
+//document.querySelector('#s').onclick = function () {  
+//  wx.scanQRCode();  
+//};  
+  // 9.1.2 扫描二维码并返回结果  
+  document.querySelector('#saoman').onclick = function () {  
+    wx.scanQRCode({  
+      needResult: 1,  
+      desc: 'scanQRCode desc',  
+      success: function (res) {  
+        alert(JSON.stringify(res));  
+      }  
+    });  
+  };  
+});  
+  
+wx.error(function (res) {  
+  alert(res.errMsg);  
+}); 
+</script>
+</html>
+
