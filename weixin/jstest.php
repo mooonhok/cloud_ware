393ed33f4cc8f89dff6b4c523aec8ec5f41ccdff
GHOSTDO <2121066061@qq.com>
2017-08-27 15:53:13 +0800

    随机页面

--- weixin/jstest.php
+++ weixin/jstest.php
@@ -1,67 +1,38 @@
 <?php
-require_once "jssdk.php";
-$jssdk = new JSSDK("wx15ef051f9f0bba92", "57ea0ee4abf4f4c6d6e38c88a289e687");
-$signPackage = $jssdk->GetSignPackage();
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/27
+ * Time: 14:31
+ */
+$num=rand(1,1000);
+echo $num;
+header('Content-type:text/html;charset=utf-8');
+if ($_COOKIE['openid'] == null) {
+    if (!isset($_GET['code'])) {
+        $appid = 'wx15ef051f9f0bba92';
+        $redirect_uri = urlencode('http://mooonhok-cloudware.daoapp.io/weixin/jstest.php');
+        $scope = 'snsapi_base';
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
+        header('Location:' . $url);
+    } else {
+        $appid = "wx15ef051f9f0bba92";
+        $secret = "57ea0ee4abf4f4c6d6e38c88a289e687";
+        $code = $_GET["code"];
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $json_obj = json_decode($output, true);
+        // echo $json_obj['openid'];
+        setcookie('openid', $json_obj['openid']);
+        header('location:http://mooonhok-cloudware.daoapp.io/weixin/payment.html?num='.$num);
+    }
+}else{
+    header('location:http://mooonhok-cloudware.daoapp.io/weixin/payment.html?num='.$num);
+}
 ?>
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-    <meta charset="UTF-8">
-    	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-        <style type="text/css">
-    	#saoman{
-    		height:60px;
-    		width:80px;
-    	}
-    	</style>
-    <title></title>
-</head>
-<body>
-   <div id='saoman'>扫码<div>
-</body>
-<script src='http://res.wx.qq.com/open/js/jweixin-1.0.0.js'></script>
-<script>
-    /*
-     * 注意：
-     * 1. 所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
-     * 2. 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
-     * 3. 常见问题及完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
-     *
-     * 开发中遇到问题详见文档“附录5-常见错误及解决办法”解决，如仍未能解决可通过以下渠道反馈：
-     * 邮箱地址：weixin-open@qq.com
-     * 邮件主题：【微信JS-SDK反馈】具体问题
-     * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
-     */
-    wx.config({
-        debug: true,
-        appId: '<?php echo $signPackage["appId"];?>',
-        timestamp: <?php echo $signPackage["timestamp"];?>,
-        nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-        signature: '<?php echo $signPackage["signature"];?>',
-        jsApiList: [
-            'checkJsApi', 'scanQRCode'
-        ]
-    });
-    wx.ready(function () { 
-  document.querySelector('#saoman').onclick = function () {  
-    wx.scanQRCode({  
-      needResult: 1,  
-      desc: 'scanQRCode desc',  
-      success: function (res) {  
-        alert(JSON.stringify(res));  
-        alert(res.resultStr);
-        var a=new Array();
-        a=res.resultStr.split(",");
-        alert(a[0]);
-        alert(a[1]);
-        alert(a[2]);
-      }  
-    });  
-  };  
-});  
-  
-wx.error(function (res) {  
-  alert(res.errMsg);  
-}); 
-</script>
-</html>
