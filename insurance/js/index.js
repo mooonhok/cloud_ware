96d58bb1684333a43c79177189cb102b3eeb4934
boycui <605955595@qq.com>
2017-09-07 16:56:23 +0800

    Signed-off-by: boycui <605955595@qq.com>

--- insurance/js/index.js
+++ insurance/js/index.js
@@ -11,15 +11,49 @@ $(document).ready(function(){
  		  	  }),
  		  	  success:function(msg){
  		  	  	console.log(msg);
- 		         for(var a=0;a<msg.insurance_rechanges.length;a++){
+ 		         for(var i=0;i<msg.insurance_rechanges.length;i++){
  		         	if(msg.insurance_rechanges[i].status==0){
- 		         		var a={'a':msg.insurance_rechanges[i].company,'b':msg.insurance_rechanges[i].pay_time,
+ 		         		var testdata={'a':msg.insurance_rechanges[i].company,'b':msg.insurance_rechanges[i].pay_time,
 	             'c':msg.insurance_rechanges[i].money,'d':'合同详情','e':'<button type="button" value="'+msg.insurance_rechanges[i].id+'" onclick="btn(this)">确认支付</button>'};
+	                $('#testtable3').yhhDataTable({
+		              'paginate':{
+			          'changeDisplayLen':true,
+			          'type':'updown',
+			          'visibleGo': true
+		 },
+		           'tbodyRow':{
+			       'zebra':true,
+			       'write':function(d){
+				       return '<tr><td>'+d.a+'</td><td>'+d.b+'</td><td>'+d.c+'</td><td>'+d.d+'</td><td>'+d.e+'</td></tr>';
+			}
+		},
+		           'tbodyData':{
+			       'enabled':true,  /*是否传入表格数据*/
+			       'source':testdata2 /*传入的表格数据*/
+		}
+	});
  		         	}else{
- 		         		var a={'a':msg.insurance_rechanges[i].company,'b':msg.insurance_rechanges[i].pay_time,
+ 		         		var testdata={'a':msg.insurance_rechanges[i].company,'b':msg.insurance_rechanges[i].pay_time,
 	             'c':msg.insurance_rechanges[i].money,'d':'','e':'已经支付'};
+	              $('#testtable3').yhhDataTable({
+		              'paginate':{
+			          'changeDisplayLen':true,
+			          'type':'updown',
+			          'visibleGo': true
+		 },
+		           'tbodyRow':{
+			       'zebra':true,
+			       'write':function(d){
+				       return '<tr><td>'+d.a+'</td><td>'+d.b+'</td><td>'+d.c+'</td><td>'+d.d+'</td><td>'+d.e+'</td></tr>';
+			}
+		},
+		           'tbodyData':{
+			       'enabled':true,  /*是否传入表格数据*/
+			       'source':testdata2 /*传入的表格数据*/
+		}
+	});
  		         	}   
-	             date2+=a;
+	             date2+=i;
                }
 		  	  },
  		  	  error:function(xhr) {
@@ -27,70 +61,70 @@ $(document).ready(function(){
  		  	  }
 		});
 
-	var testdata2 = [date2];
-	$('#testtable3').yhhDataTable({
-		'paginate':{
-			'changeDisplayLen':true,
-			'type':'updown',
-			'visibleGo': true
-		},
-		'tbodyRow':{
-			'zebra':true,
-			'write':function(d){
-				return '<tr><td>'+d.a+'</td><td>'+d.b+'</td><td>'+d.c+'</td><td>'+d.d+'</td><td>'+d.e+'</td></tr>';
-			}
-		},
-		'tbodyData':{
-			'enabled':true,  /*是否传入表格数据*/
-			'source':testdata2 /*传入的表格数据*/
-		}
-	});
+	
+	// $('#testtable3').yhhDataTable({
+	// 	'paginate':{
+	// 		'changeDisplayLen':true,
+	// 		'type':'updown',
+	// 		'visibleGo': true
+	// 	},
+	// 	'tbodyRow':{
+	// 		'zebra':true,
+	// 		'write':function(d){
+	// 			 return '<tr><td>'+d.a+'</td><td>'+d.b+'</td><td>'+d.c+'</td><td>'+d.d+'</td><td>'+d.e+'</td></tr>';
+	// 		}
+	// 	},
+	// 	'tbodyData':{
+	// 		'enabled':true,  /*是否传入表格数据*/
+	// 		'source':testdata2 /*传入的表格数据*/
+	// 	}
+	// });
 	//历史保险记录
-	$.ajax({
-		  	  url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurances?tenant_id=1",
- 		  	  dataType:'json',
-		  	  type:'get',
-		  	  contentType:"application/json;charset=utf-8",
- 		  	  data:JSON.stringify({
- 		  	  }),
- 		  	  success:function(msg){
- 		         for(var a=0;a<msg.insurances.length;a++){
-	             var a={'a':msg.insurances[i].plate_number,'b':msg.insurance[i].driver_name,'c':msg.insurances[i].driver_phone,
-	             'd':'<button type="button" value="'+msg.insurances[i].id+'" onclick="but(this)">详情</button>','e':msg.insurances[i].insurance_price,'f':msg.insurance[i].insurance_money,'g':msg.insurance[i].time};
-	             date3+=a;
-               }
-		  	  },
- 		  	  error:function(xhr) {
-		  	  	alert(xhr.responseText);
- 		  	  }
-		});
-	var testdata3 = {'code':'000','data':[date3]};
-	$('#testtable4').yhhDataTable({
-		'paginate':{
-			'changeDisplayLen':true,
-			'type':'updown',
-			'visibleGo': true
-		},
-		'tbodyRow':{
-			'zebra':true,
-			'write':function(d){
-				return '<tr><td>'+d.a+'</td><td>'+d.b+'</td><td>'+d.c+'</td><td>'+d.d+'</td><td>'+d.e+'</td><td>'+d.f+'</td><td>'+d.g+'</td></tr>';
+	// $.ajax({
+	// 	  	  url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurances?tenant_id=1",
+ // 		  	  dataType:'json',
+	// 	  	  type:'get',
+	// 	  	  contentType:"application/json;charset=utf-8",
+ // 		  	  data:JSON.stringify({
+ // 		  	  }),
+ // 		  	  success:function(msg){
+ // 		         for(var a=0;a<msg.insurances.length;a++){
+	//              var a={'a':msg.insurances[i].plate_number,'b':msg.insurance[i].driver_name,'c':msg.insurances[i].driver_phone,
+	//              'd':'<button type="button" value="'+msg.insurances[i].id+'" onclick="but(this)">详情</button>','e':msg.insurances[i].insurance_price,'f':msg.insurance[i].insurance_money,'g':msg.insurance[i].time};
+	//              date3+=a;
+ //               }
+	// 	  	  },
+ // 		  	  error:function(xhr) {
+	// 	  	  	alert(xhr.responseText);
+ // 		  	  }
+	// 	});
+	// var testdata3 = {'code':'000','data':[date3]};
+	// $('#testtable4').yhhDataTable({
+	// 	'paginate':{
+	// 		'changeDisplayLen':true,
+	// 		'type':'updown',
+	// 		'visibleGo': true
+	// 	},
+	// 	'tbodyRow':{
+	// 		'zebra':true,
+	// 		'write':function(d){
+	// 			return "<tr><td>'+d.a+'</td><td>'+d.b+'</td><td>'+d.c+'</td><td>'+d.d+'</td><td>'+d.e+'</td><td>'+d.f+'</td><td>'+d.g+'</td></tr>";
 				
-			}
-		},
-		'tbodyData':{
-			'enabled':true,  /*是否传入表格数据*/
-			'source':testdata3 /*传入的表格数据*/
-		},
-		'backDataHandle':function(d){
-			if (d.code == '000'){
-				return d.data;
-			} else {
-				alert('出错信息');
-				return [];
-			}
-		}
-	});
+	// 		}
+	// 	},
+	// 	'tbodyData':{
+	// 		'enabled':true,  /*是否传入表格数据*/
+	// 		'source':testdata3 /*传入的表格数据*/
+	// 	},
+	// 	'backDataHandle':function(d){
+	// 		if (d.code == '000'){
+	// 			return d.data;
+	// 		} else {
+	// 			alert('出错信息');
+	// 			return [];
+	// 		}
+	// 	}
+	// });
 	
 	/*更新表格*/ 
 	var refreshTable = function(data,page){
