ac1076f8af15f2357e16598533d30c5a1190ea56
boycui <605955595@qq.com>
2017-09-09 14:23:14 +0800

    Signed-off-by: boycui <605955595@qq.com>

--- insurance/js/index1.js
+++ insurance/js/index1.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-	var a = "";
-	var b = "";
-	var c = "";
-	var d = "";
-	var e = "";
+	// var a = "";
+	// var b = "";
+	// var c = "";
+	// var d = "";
+	// var e = "";
 	var testdata2 = "";
 	$.ajax({
 		url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurance_rechanges?tenant_id=1",
@@ -16,31 +16,33 @@ $(document).ready(function(){
  		    for(var i=0;i<msg.insurance_rechanges.length;i++){
 
  		    	console.log(msg.insurance_rechanges.length);
- 		    	    a += msg.insurance_rechanges[i].company+",";
- 		    		b += msg.insurance_rechanges[i].pay_time+",";
- 		    		c += msg.insurance_rechanges[i].money+",";
- 		    		d += '合同详情'+",";
- 		    		e += '<button type="button" value="'+msg.insurance_rechanges[i].id+'" onclick="btn(this)">确认支付</button>'+",";
- 		    		a1 = a.split(",")
- 		    		b1 = b.split(",")
- 		    		c1 = c.split(",")
- 		    		d1 = d.split(",")
- 		    		e1 = e.split(",")
- 		    		console.log(a1)
- 		    		console.log(b1)
- 		    		console.log(c1)
- 		    		console.log(d1)
- 		    		console.log(e1)
- 		    		testdata2 = [{'a':a1,'b':b1,'c':c1,'d':d1,'e':e1}]; 
+ 		    	 //    a += msg.insurance_rechanges[i].company+",";
+ 		    		// b += msg.insurance_rechanges[i].pay_time+",";
+ 		    		// c += msg.insurance_rechanges[i].money+",";
+ 		    		// d += '合同详情'+",";
+ 		    		// e += '<button type="button" value="'+msg.insurance_rechanges[i].id+'" onclick="btn(this)">确认支付</button>'+",";
+ 		    		// a1 = a.split(",")
+ 		    		// b1 = b.split(",")
+ 		    		// c1 = c.split(",")
+ 		    		// d1 = d.split(",")
+ 		    		// e1 = e.split(",")
+ 		    		// console.log(a1)
+ 		    		// console.log(b1)
+ 		    		// console.log(c1)
+ 		    		// console.log(d1)
+ 		    		// console.log(e1)
  		    		
+ 		    		testdata2 += [{'a':msg.insurance_rechanges[i].company,'b':msg.insurance_rechanges[i].pay_time,'c':msg.insurance_rechanges[i].money,'d':'合同详情','e':'<button type="button" value="'+msg.insurance_rechanges[i].id+'" onclick="btn(this)">确认支付</button>'}]; 
+ 		    		console.log(testdata2)
  		    	if(msg.insurance_rechanges[i].status!=0){	    		
-                    e += '<button type="button" value="'+msg.insurance_rechanges[i].id+'" onclick="btn(this)">已支付</button>';
+                    testdata2 += [{'a':msg.insurance_rechanges[i].company,'b':msg.insurance_rechanges[i].pay_time,'c':msg.insurance_rechanges[i].money,'d':'合同详情','e':'<button type="button" value="'+msg.insurance_rechanges[i].id+'" onclick="btn(this)">已支付</button>'}]; 
+ 		    		console.log(testdata2)
  		    	}
- 		    	console.log(a);
- 		    	console.log(b);
- 		    	console.log(c);
- 		    	console.log(d);
- 		    	console.log(e);
+ 		    	// console.log(a);
+ 		    	// console.log(b);
+ 		    	// console.log(c);
+ 		    	// console.log(d);
+ 		    	// console.log(e);
  		    	 // var testdata2=[{'a':a,'b':b,'c':c,'d':d,'e':e},]; 
  		    	 console.log(testdata2);
  		    }
