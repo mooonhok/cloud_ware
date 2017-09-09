f5bdfe6ea4547fcec738d3ec8313ae762423c7cc
boycui <605955595@qq.com>
2017-09-09 17:05:08 +0800

    Signed-off-by: boycui <605955595@qq.com>

--- insurance/js/index1.js
+++ insurance/js/index1.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 			if (d.status==0) {
 				return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" value="'+d.status+'" onclick="btn(this)">确认支付</button></td></tr>';
 			}else{
-		     return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" value="'+d.status+'" onclick="btn(this)">已支付</button></td></tr>';
+		     return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" disabled="disabled" value="'+d.status+'">已支付</button></td></tr>';
 			}
 			}
 		},
@@ -36,6 +36,53 @@ $(document).ready(function(){
  		  	  }
 
 	})
+$(".hunt").on("click",function(){
+	var cit = $(".cit option").value
+	if(cit=""){
+		alert("请选择城市")
+	}else{
+		$.ajax({
+		    url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurance_rechanges?tenant_id=1",
+		    dataType:"json",
+		    contentType:"application/json;charset=utf-8",
+		    data:JSON.stringify({
+ 		}),
+		   success:function(msg){
+		   var fcity = msg.insurance_rechanges.from_city_id;
+		   var rcity = msg.insurance_rechanges.receive_city_id;
+		   $('#testtable3').yhhDataTable({
+		  'paginate':{
+			'changeDisplayLen':true,
+			'type':'updown',
+			'visibleGo': true
+		},
+		    'tbodyRow':{
+			'zebra':true,
+			'write':function(d){
+			if (cit==frcity&&d.status==0) {
+				return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" value="'+d.status+'" onclick="btn(this)">确认支付</button></td></tr>';
+			}else if(cit==frcity&&d.status!=0){
+		     return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" disabled="disabled" value="'+d.status+'">已支付</button></td></tr>';
+			}
+			}
+		},
+		    'tbodyData':{
+			'enabled':true,  /*是否传入表格数据*/
+			'source':testdata2 /*传入的表格数据*/
+		}
+	});
+ 		         
+ 		}, error:function(xhr) {
+		  	  	alert(xhr.responseText);
+ 		  	  }
+		   
+		})
+	}
+})
+
+
+
+
 	var refreshTable = function(data,page){
 			if ($.isEmptyObject(data)) data = {};
 			var toData = {
