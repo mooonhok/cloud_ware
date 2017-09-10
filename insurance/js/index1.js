ebf231f82bed0d940eb9f5c101b0c7f020696bde
boycui <605955595@qq.com>
2017-09-10 13:33:04 +0800

    Signed-off-by: boycui <605955595@qq.com>

--- insurance/js/index1.js
+++ insurance/js/index1.js
@@ -1,6 +1,17 @@
+(function($) {
+			$.getUrlParam = function(name) {
+				var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+				var r = window.location.search.substr(1).match(reg);
+				if(r != null) return unescape(r[2]);
+				return null;
+			}
+		})(jQuery);
+
 $(document).ready(function(){
+	var city_id = $.getUrlParam("city_id");
+	var company = $.getUrlParam("company");
 	$.ajax({
-		url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurance_rechanges?tenant_id=1",
+		url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurance_rechanges?city_id="+city_id+"&company="+company,
 		dataType:"json",
 		contentType:"application/json;charset=utf-8",
 		data:JSON.stringify({
@@ -44,44 +55,9 @@ $(".hunt").on("click",function(){
 	if(cit==0){
 		alert("请选择城市")
 	}else{
-		$("#testtable3 tbody").html("");
-		$.ajax({
-		    url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurance_rechanges?city_id="+cit,
-		    dataType:"json",
-		    contentType:"application/json;charset=utf-8",
-		    data:JSON.stringify({
- 		}),
-		   success:function(msg){
-		   
-		   var testdata4 =msg.insurance_rechanges;
-		   console.log(testdata4);
-		   $('#testtable3').yhhDataTable({
-		//  'paginate':{
-		// 	'changeDisplayLen':true,
-		// 	'type':'updown',
-		// 	'visibleGo': true
-		// },
-		    'tbodyRow':{
-			'zebra':true,
-			'write':function(d){
-			if (d.status==0) {
-				return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" value="'+d.status+'" onclick="btn(this)">确认支付</button></td></tr>';
-			}else if(d.status!=0){
-		     return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" disabled="disabled" value="'+d.status+'">已支付</button></td></tr>';
-			}
-		  }
-		},
-		    'tbodyData':{
-			'enabled':true,  /*是否传入表格数据*/
-			'source':testdata4 /*传入的表格数据*/
-		}
-	});
- 		         
- 		}, error:function(xhr) {
-		  	  	alert(xhr.responseText);
- 		  	  }
-		   
-		})
+		window.location.href="http://mooonhok-cloudware.daoapp.io/insurance/safe.html?city_id="+city_id;
+		
+		
 	}
 })
 
