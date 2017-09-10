accf2357d88f8e21ceab3bf2411eeac93347f738
boycui <605955595@qq.com>
2017-09-10 10:54:04 +0800

    Signed-off-by: boycui <605955595@qq.com>

--- insurance/js/index1.js
+++ insurance/js/index1.js
@@ -44,6 +44,28 @@ $(".hunt").on("click",function(){
 	if(cit==0){
 		alert("请选择城市")
 	}else{
+		 $('#testtable3').yhhDataTable({
+		  'paginate':{
+		  	'enabled':true,
+			'changeDisplayLen':true,
+			'type':'updown',
+			'visibleGo': true
+		},
+		    'tbodyRow':{
+			'zebra':true,
+			'write':function(d){
+			if (d.status==0) {
+				return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" value="'+d.status+'" onclick="btn(this)">确认支付</button></td></tr>';
+			}else{
+		     return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" disabled="disabled" value="'+d.status+'">已支付</button></td></tr>';
+			}
+			}
+		},
+		    'tbodyData':{
+			'enabled':false,  /*是否传入表格数据*/
+			'source':testdata2 /*传入的表格数据*/
+		}
+	});
 		$.ajax({
 		    url:"http://mooonhok-cloudware.daoapp.io/rechange_insurance.php/insurance_rechanges?city_id="+cit,
 		    dataType:"json",
@@ -68,7 +90,7 @@ $(".hunt").on("click",function(){
 			}else if(d.status!=0){
 		     return '<tr><td>'+d.company+'</td><td>'+d.pay_time+'</td><td>'+d.money+'</td><td>'+d.id+'</td><td><button type="button" disabled="disabled" value="'+d.status+'">已支付</button></td></tr>';
 			}
-			}
+		  }
 		},
 		    'tbodyData':{
 			'enabled':true,  /*是否传入表格数据*/
