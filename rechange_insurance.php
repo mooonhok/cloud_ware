a7c103c13f062b7d7f72d2d3e465ccdf9e5596cb
mooonhok <1026413232@qq.com>
2017-09-20 10:32:34 +0800

    修改

--- rechange_insurance.php
+++ rechange_insurance.php
@@ -14,8 +14,8 @@ require 'connect.php';
 $app = new \Slim\Slim();
 //登录方法
 $app->post('/userlogin',function ()use($app){
+    $app->response->headers->set('Access-Control-Allow-Headers','Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With');
     $app->response->headers->set('Access-Control-Allow-Origin','*');
-    $app->response->headers->set('Access-Control-Allow-Headers','Content-Type');
     $app->response->headers->set('Access-Control-Allow-Methods','POST');
     $app->response->headers->set('Content-Type','application/json');
     $database=localhost();
