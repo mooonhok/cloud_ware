6c6d9facae8b69a2e7de904f885d1525e5011620
mooonhok <1026413232@qq.com>
2017-09-21 15:26:55 +0800

    修改

--- menu.php
+++ menu.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET,POST,PUT,DELETE,OPTIONS");
 header('Content-type:text/html;charset=utf-8');
 $str=$_SERVER["QUERY_STRING"];
 $arr=explode("=",$str);
