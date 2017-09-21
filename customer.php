61e58e843f23e777eced42b53d3ffbe65bed2553
mooonhok <1026413232@qq.com>
2017-09-21 15:36:43 +0800

    修改

--- customer.php
+++ customer.php
@@ -387,14 +387,14 @@ $app->get('/wx_openid',function()use($app){
 $app->options('/wxaddress',function()use($app){
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
-    $app->response->headers->set("Access-Control-Allow-Methods", "POST");
+    $app->response->headers->set("Access-Control-Allow-Methods", "GET");
   });
 
 //微信获得所有地址
-$app->post('/wxaddress',function()use($app){
+$app->get('/wxaddress',function()use($app){
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
-    $app->response->headers->set("Access-Control-Allow-Methods", "POST");
+    $app->response->headers->set("Access-Control-Allow-Methods", "GET");
     $database=localhost();
     $tenant_id=$app->request->headers->get('tenant-id');
     $wx_openid=$app->request->get('wx_openid');
