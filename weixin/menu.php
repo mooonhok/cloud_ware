4574bf00a235ed43a961bd06ad831d60ffb35fcd
mooonhok <1026413232@qq.com>
2017-09-26 09:36:09 +0800

    修改

--- weixin/menu.php
+++ weixin/menu.php
@@ -44,8 +44,6 @@ if ($_COOKIE['openid'.$tenant_id] == null) {
            header('location:http://api.uminfo.cn/weixin/query.php?tenant_id='.$tenant_id);
         }else if($page==1){
             header('location:http://api.uminfo.cn/weixin/send.html?tenant_id='.$tenant_id);
-        }else{
-        	echo '<script>window.close();</script>'; 
         }
     }
 }else{
@@ -63,8 +61,6 @@ if ($_COOKIE['openid'.$tenant_id] == null) {
            header('location:http://api.uminfo.cn/weixin/query.php?tenant_id='.$tenant_id);
         }else if($page==1){
             header('location:http://api.uminfo.cn/weixin/send.html?tenant_id='.$tenant_id);
-        }else{
-        	echo '<script>window.close();</script>'; 
         }
 }
 ?>
