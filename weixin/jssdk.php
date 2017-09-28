e8cd69a8d5796b67d11c6fab58047e2705f243ca
mooonhok <1026413232@qq.com>
2017-09-28 15:13:22 +0800

    修改

--- weixin/jssdk.php
+++ weixin/jssdk.php
@@ -49,8 +49,8 @@ class JSSDK {
     if ($data->expire_time < time()) {
       $accessToken = $this->getAccessToken();
       // 如果是企业号用以下 URL 获取 ticket
-      $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
-      //$url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
+      //$url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
+      $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
       $res = json_decode($this->httpGet($url));
       $ticket = $res->ticket;
       if ($ticket) {
