2f9be8edd01dc2009c0477e688ff10f51fa384a4
mooonhok <1026413232@qq.com>
2017-10-13 11:04:28 +0800

    修改

--- lorry_nedb.php
+++ lorry_nedb.php
@@ -238,7 +238,7 @@ $app->post('/uploadLorry',function()use($app) {
         $shijian = time();
         $name1 = $shijian . $name1;
         move_uploaded_file($_FILES["driving_license"]["tmp_name"], "lorry/" . $name1);
-        $array['driving_license']="http://api.uminfo.cn/".$name1;
+        $array['driving_license']="http://api.uminfo.cn/lorry/".$name1;
     }
     if(isset($_FILES["vehicle_travel_license"])){
         $name2 = $_FILES["vehicle_travel_license"]["name"];
@@ -246,7 +246,7 @@ $app->post('/uploadLorry',function()use($app) {
         $shijian = time();
         $name2 = $shijian . $name2;
         move_uploaded_file($_FILES["vehicle_travel_license"]["tmp_name"], "lorry/" . $name2);
-        $array['vehicle_travel_license']="http://api.uminfo.cn/".$name2;
+        $array['vehicle_travel_license']="http://api.uminfo.cn/lorry/".$name2;
     }
 
     if($tenant_id!=null||$tenant_id!=''){
