0961ad206116366a8d5f5ce186606206c723af65
mooonhok <1026413232@qq.com>
2017-10-14 15:03:09 +0800

    修改

--- tenant_insurancepolicy.php
+++ tenant_insurancepolicy.php
@@ -22,7 +22,9 @@ $app->post('/upload',function()use($app) {
         $name3 = iconv("UTF-8", "UTF-8", $name3);
         $shijian = time();
         $name3 = $shijian . $name3;
-        move_uploaded_file($_FILES["file1"]["tmp_name"], "tenant/insurance/" . $name3);
+//        move_uploaded_file($_FILES["file1"]["tmp_name"], "tenant/insurance/" . $name3);
+    $url="http://files.uminfo.cn:8000/";
+    move_uploaded_file($_FILES["file1"]["tmp_name"], $url . $name3);
         if ($tenant_id != null || $tenant_id != '') {
             $insertStatement = $database->insert(array('tenant_id', 'tenant_insurancepolicy'))
                 ->into('tenant_insurancepolicy')
