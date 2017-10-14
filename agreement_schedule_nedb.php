9b5c9e9c3e5c4f1bf4e01b857dfa9b42e06a68bf
mooonhok <1026413232@qq.com>
2017-10-14 13:40:07 +0800

    修改

--- agreement_schedule_nedb.php
+++ agreement_schedule_nedb.php
@@ -52,9 +52,43 @@ $app->get('/getAgreementSchedules0',function()use($app) {
     if($tenant_id!=''||$tenant_id!=null){
         $selectStatement = $database->select()
             ->from('agreement_schedule')
-            ->where('tenant_id','=',$tenant_id);
+            ->join('agreement','agreement.agreement_id','=','agreement_schedule.agreement_id','INNER')
+            ->join('scheduling','agreement_schedule.scheduling_id','=','scheduling.scheduling_id','INNER')
+            ->join('lorry','lorry.lorry_id','=','agreement.secondparty_id','INNER')
+            ->where('agreement.tenant_id','=',$tenant_id)
+            ->where('scheduling.tenant_id','=',$tenant_id)
+            ->where('lorry.tenant_id','=',$tenant_id)
+            ->where('agreement_schedule.tenant_id','=',$tenant_id)
+            ->where('agreement_schedule.exist','=',0)
+            ->where('agreement.exist','=',0);
         $stmt = $selectStatement->execute();
         $data = $stmt->fetchAll();
+        for($i=0;$i<count($data);$i++){
+            $selectStatement = $database->select()
+                ->from('city')
+                ->where('id', '=', $data[$i]['send_city_id']);
+            $stmt = $selectStatement->execute();
+            $data1 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('province')
+                ->where('id', '=', $data1['pid']);
+            $stmt = $selectStatement->execute();
+            $data3 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('city')
+                ->where('id', '=', $data[$i]['receive_city_id']);
+            $stmt = $selectStatement->execute();
+            $data2 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('province')
+                ->where('id', '=', $data2['pid']);
+            $stmt = $selectStatement->execute();
+            $data4 = $stmt->fetch();
+            $data[$i]['send_city']=$data1;
+            $data[$i]['receive_city']=$data2;
+            $data[$i]['send_province']=$data3;
+            $data[$i]['receive_province']=$data4;
+        }
         echo json_encode(array("result" => "1", "desc" => 'success','agreement_schedules'=>$data));
     }else{
         echo json_encode(array("result" => "2", "desc" => "缺少租户id"));
@@ -68,10 +102,43 @@ $app->get('/getAgreementSchedules1',function()use($app) {
     if($tenant_id!=''||$tenant_id!=null){
         $selectStatement = $database->select()
             ->from('agreement_schedule')
-            ->where('tenant_id','=',$tenant_id)
-            ->where('exist','=',0);
+            ->join('agreement','agreement.agreement_id','=','agreement_schedule.agreement_id','INNER')
+            ->join('scheduling','agreement_schedule.scheduling_id','=','scheduling.scheduling_id','INNER')
+            ->join('lorry','lorry.lorry_id','=','agreement.secondparty_id','INNER')
+            ->where('agreement.tenant_id','=',$tenant_id)
+            ->where('scheduling.tenant_id','=',$tenant_id)
+            ->where('lorry.tenant_id','=',$tenant_id)
+            ->where('agreement_schedule.tenant_id','=',$tenant_id)
+            ->where('agreement_schedule.exist','=',0)
+            ->where('agreement.exist','=',0);
         $stmt = $selectStatement->execute();
         $data = $stmt->fetchAll();
+        for($i=0;$i<count($data);$i++){
+            $selectStatement = $database->select()
+                ->from('city')
+                ->where('id', '=', $data[$i]['send_city_id']);
+            $stmt = $selectStatement->execute();
+            $data1 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('province')
+                ->where('id', '=', $data1['pid']);
+            $stmt = $selectStatement->execute();
+            $data3 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('city')
+                ->where('id', '=', $data[$i]['receive_city_id']);
+            $stmt = $selectStatement->execute();
+            $data2 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('province')
+                ->where('id', '=', $data2['pid']);
+            $stmt = $selectStatement->execute();
+            $data4 = $stmt->fetch();
+            $data[$i]['send_city']=$data1;
+            $data[$i]['receive_city']=$data2;
+            $data[$i]['send_province']=$data3;
+            $data[$i]['receive_province']=$data4;
+        }
         echo json_encode(array("result" => "1", "desc" => 'success','agreement_schedules'=>$data));
     }else{
         echo json_encode(array("result" => "2", "desc" => "缺少租户id"));
