7e6208e81dd4b6e06bb785a2192e929d86dc77f7
mooonhok <1026413232@qq.com>
2017-10-16 09:19:18 +0800

    修改

--- wx_interface.php
+++ wx_interface.php
@@ -24,7 +24,7 @@ class wechatCallbackapiTest
     {
         $echoStr = $_GET["echostr"];
 
-        if($this->checkSignature()){
+        if ($this->checkSignature()) {
             echo $echoStr;
             exit;
         }
@@ -79,7 +79,7 @@ class wechatCallbackapiTest
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
         //extract post data
-        if (!empty($postStr)){
+        if (!empty($postStr)) {
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
@@ -96,7 +96,7 @@ class wechatCallbackapiTest
                                 <Articles>  
                                 <item>  
                                 <Title><![CDATA[%s]]></Title>   
-                                <Description><![CDATA[%s]]></Description> 
+                                <Description><![CDATA[%s]]></Description>  
                                 <PicUrl><![CDATA[%s]]></PicUrl>  
                                 <Url><![CDATA[%s]]></Url>  
                                 </item>   
@@ -107,25 +107,24 @@ class wechatCallbackapiTest
 						<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
 							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
+							<Content><![CDATA[%s]]></Content> 
 							<FuncFlag>0</FuncFlag>
 							</xml>";
-            if ($ev == "subscribe"){
-         //       echo '123456';
-                $resultStr = sprintf($newsTpl, $fromUsername, $toUsername, $time, 'news',$ArticleCount,"公司介绍","万事鑫公司专业从事物流运输行业","http://api.uminfo.cn/timg.jpg","http://api.uminfo.cn/weixin/jiangsuyouming.html");
+            if ($ev == "subscribe") {
+                //       echo '123456';
+                $resultStr = sprintf($newsTpl, $fromUsername, $toUsername, $time, 'news', $ArticleCount, "公司介绍", "万事鑫公司专业从事物流运输行业", "http://api.uminfo.cn/timg.jpg", "http://api.uminfo.cn/weixin/jiangsuyouming.html");
                 echo $resultStr;
             }
 
-            if(!empty( $keyword ))
-            {
+            if (!empty($keyword)) {
                 $msgType = "text";
                 $contentStr = "请联系客服电话：15862790779";
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                 echo $resultStr;
-            }else{
+            } else {
                 echo "Input something...";
             }
-        }else {
+        } else {
             echo "";
             exit;
         }
@@ -141,15 +140,42 @@ class wechatCallbackapiTest
         $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
 
-        if( $tmpStr == $signature ){
+        if ($tmpStr == $signature) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-}
 
+//业务消息方法
+    public function responseMsg2($openid,$order_id)
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+
+        $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $keyword = trim($postObj->Content);
+        $ev = $postObj->Event;
+        $time = time();
+        $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+        $msgType = "text";
+        $contentStr = "您的订单已经生成，正在等待前台确认,订单号为".$order_id;
+        $resultStr = sprintf($textTpl, $fromUsername, $openid, $time, $msgType, $contentStr);
+        echo $resultStr;
+    }
+}
 ?>
