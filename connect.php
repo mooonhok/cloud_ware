c9dc1f90f17b64aa7d33a8934834acaa260494dd
mooonhok <1026413232@qq.com>
2017-10-16 14:15:49 +0800

    修改

--- connect.php
+++ connect.php
@@ -7,17 +7,16 @@
  */
 use Slim\PDO\Database;
 function connect(){
-    $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "	db2.daocloudinternal.io");
-    $databaseName = env("MYSQL_INSTANCE_NAME", "temp_db");
-    $username = env("MYSQL_USERNAME", "root");
-    $password = env("MYSQL_PASSWORD", "Gd6lhOSsY");
-//    $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "172.17.16.8");
-//    $databaseName = env("MYSQL_INSTANCE_NAME", "cloud_ware");
+//    $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "	db2.daocloudinternal.io");
+//    $databaseName = env("MYSQL_INSTANCE_NAME", "temp_db");
 //    $username = env("MYSQL_USERNAME", "root");
-//    $password = env("MYSQL_PASSWORD", "jsym_20170607");
-//    $port=env(60212,3306);
-//    $database=new database("mysql:host=".$serverName.";port=".$port.";dbname=".$databaseName.";charset=utf8",$username,$password);
-    $database=new database("mysql:host=".$serverName.";port=60203;dbname=".$databaseName.";charset=utf8",$username,$password);
+//    $password = env("MYSQL_PASSWORD", "Gd6lhOSsY");
+    $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "172.17.16.8");
+    $databaseName = env("MYSQL_INSTANCE_NAME", "cloud_ware");
+    $username = env("MYSQL_USERNAME", "root");
+    $password = env("MYSQL_PASSWORD", "jsym_20170607");
+    $port=env(60212,3306);
+    $database=new database("mysql:host=".$serverName.";port=".$port.";dbname=".$databaseName.";charset=utf8",$username,$password);
 
     return  $database;
 }
