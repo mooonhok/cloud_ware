6f7b836c7b0cf5f6bf6adbf5c0844e7df46fd20c
mooonhok <1026413232@qq.com>
2017-10-17 08:58:50 +0800

    修改

--- wx_test.php
+++ wx_test.php
@@ -8,7 +8,7 @@
 
 require 'Slim/Slim.php';
 require 'connect.php';
-
+require  'wx_interface.php';
 
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
@@ -25,6 +25,17 @@ $app->get('/tenant',function()use($app){
     }else{
         echo json_encode(array('result'=>'1','desc'=>'尚未有数据','tenant'=>''));
     }
+});
+
+$app->post('/testme',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $body=$app->request->getBody();
+    $body=json_decode($body);
+    $openid=$body->openid;
+    $orderid=$body->orderid;
+    $wechatObj = new wechatCallbackapiTest();
+    $wechatObj->responseMsg2($openid,$orderid);
 });
 
 $app->run();
