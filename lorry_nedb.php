d038fb958e771d7d6e8b02fcf36c6903240a1f4b
mooonhok <1026413232@qq.com>
2017-10-20 13:30:58 +0800

    修改

--- lorry_nedb.php
+++ lorry_nedb.php
@@ -136,22 +136,40 @@ $app->get('/getLorrys1',function()use($app){
     }
 });
 
-$app->get('/limitLorrys',function()use($app){
+$app->get('/limitLorrys1',function()use($app){
     $app->response->headers->set('Content-Type', 'application/json');
     $tenant_id = $app->request->headers->get("tenant-id");
     $database = localhost();
     $size= $app->request->get('size');
     $offset= $app->request->get('offset');
-    $flag=$app->request->get('flag');
     if($tenant_id!=null||$tenant_id!=''){
         $selectStatement = $database->select()
             ->from('lorry')
             ->leftJoin('lorry_type','lorry_type.lorry_type_id','=','lorry.lorry_type_id')
             ->where('lorry.exist', '=', 0)
             ->where('lorry.tenant_id', '=', $tenant_id)
-            ->where('lorry.flag', '=', $flag)
             ->orderBy('lorry.lorry_id','desc')
             ->limit((int)$size,(int)$offset);
+        $stmt = $selectStatement->execute();
+        $data = $stmt->fetchAll();
+        echo json_encode(array("result" => "0", "desc" => "success",'lorrys'=>$data));
+    }else{
+        echo json_encode(array("result" => "1", "desc" => "缺少租户id"));
+    }
+});
+
+$app->get('/getLorrys3',function()use($app){
+    $app->response->headers->set('Content-Type', 'application/json');
+    $tenant_id = $app->request->headers->get("tenant-id");
+    $flag=$app->request->get('flag');
+    $database = localhost();
+    if($tenant_id!=null||$tenant_id!=''){
+        $selectStatement = $database->select()
+            ->from('lorry')
+            ->leftJoin('lorry_type','lorry_type.lorry_type_id','=','lorry.lorry_type_id')
+            ->where('lorry.exist', '=', 0)
+            ->where('lorry.flag', '=', $flag)
+            ->where('lorry.tenant_id', '=', $tenant_id);
         $stmt = $selectStatement->execute();
         $data = $stmt->fetchAll();
         echo json_encode(array("result" => "0", "desc" => "success",'lorrys'=>$data));
