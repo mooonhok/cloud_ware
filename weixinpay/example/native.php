45c4b61b78557fac7ba4a1469195eaedd1d960d5
mooonhok <1026413232@qq.com>
2017-10-23 16:48:29 +0800

    修改

--- weixinpay/example/native.php
+++ weixinpay/example/native.php
@@ -18,7 +18,7 @@ $input = new WxPayUnifiedOrder();
 $input->SetBody("test");
 $input->SetAttach("test");
 $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-$input->SetTotal_fee("60000");
+$input->SetTotal_fee("1");
 $input->SetTime_start(date("YmdHis"));
 $input->SetTime_expire(date("YmdHis", time() + 600));
 $input->SetGoods_tag("test");
