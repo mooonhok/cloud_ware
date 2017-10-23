476684e2a2058d0c4f1ee64f3c79aa10b3d8c113
mooonhok <1026413232@qq.com>
2017-10-23 15:09:33 +0800

    修改

--- weixinpay/example/native.php
+++ weixinpay/example/native.php
@@ -16,8 +16,8 @@ require_once 'log.php';
  * 5、支付完成之后，微信服务器会通知支付成功
  * 6、在支付成功通知中需要查单确认是否真正支付成功（见：notify.php）
  */
-$notify = new NativePay();
-$url1 = $notify->GetPrePayUrl("123456789");
+//$notify = new NativePay();
+//$url1 = $notify->GetPrePayUrl("123456789");
 
 //模式二
 /**
@@ -49,9 +49,9 @@ $url2 = $result["code_url"];
     <title>微信支付样例-退款</title>
 </head>
 <body>
-	<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式一</div><br/>
-	<img alt="模式一扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=<?php echo urlencode($url1);?>" style="width:150px;height:150px;"/>
-	<br/><br/><br/>
+<!--	<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式一</div><br/>-->
+<!--	<img alt="模式一扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=--><?php //echo urlencode($url1);?><!--" style="width:150px;height:150px;"/>-->
+<!--	<br/><br/><br/>-->
 	<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式二</div><br/>
 	<img alt="模式二扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=<?php echo urlencode($url2);?>" style="width:150px;height:150px;"/>
 	
