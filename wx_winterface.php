3e83de6352b6d7d38fcf27ae2dd79d8012a6a2a4
mooonhok <1026413232@qq.com>
2017-10-25 13:41:42 +0800

    修改

---
+++ wx_winterface.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/10/25
+ * Time: 13:31
+ */
+
+//define your token
+define("TOKEN", "youming");
+$wechatObj = new wechatCallbackapiTest();
+if($_GET['echostr']){
+    $wechatObj->valid();//如果发来了echostr则进行验证
+}else{
+    $wechatObj->responseMsg(); //如果没有echostr，则返回消息
+}
+
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        if ($this->checkSignature()) {
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if (!empty($postStr)) {
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $ev = $postObj->Event;
+            $time = time();
+            $ArticleCount = 1;
+            $newsTpl = "<xml>  
+                                <ToUserName><![CDATA[%s]]></ToUserName>  
+                                <FromUserName><![CDATA[%s]]></FromUserName>  
+                                <CreateTime>%s</CreateTime>  
+                                <MsgType><![CDATA[%s]]></MsgType>  
+                                <ArticleCount>%s</ArticleCount>  
+                                <Articles>  
+                                <item>  
+                                <Title><![CDATA[%s]]></Title>   
+                                <Description><![CDATA[%s]]></Description>  
+                                <PicUrl><![CDATA[%s]]></PicUrl>  
+                                <Url><![CDATA[%s]]></Url>  
+                                </item>   
+                                </Articles>  
+                                </xml>";
+            $textTpl = "<xml>
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content> 
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+            $imageTpl = "<xml>
+            <ToUserName><![CDATA[toUser]]></ToUserName>
+            <FromUserName><![CDATA[fromUser]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[image]]></MsgType>
+            <Image>
+            <MediaId><![CDATA[media_id]]></MediaId>
+            </Image>
+            </xml>";
+
+            if ($ev == "subscribe") {
+                $resultStr = sprintf($newsTpl, $fromUsername, $toUsername, $time, 'news',
+                    $ArticleCount,'公司简介','江苏酉铭信息技术有限公司','',
+                    '');
+                echo $resultStr;
+            }
+            if (!empty($keyword)) {
+                if($keyword=="联系我们"){
+                    $msgType = "text";
+                    $contentStr = "请联系客服电话：15365580443";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
+                }else{
+                    $msgType = "text";
+                    $contentStr = "请查看菜单";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
+                }
+            } else {
+                echo "Input something...";
+            }
+        } else {
+            echo "";
+            exit;
+        }
+    }
+
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+?>
