ba5d3136492a8e128f7115b3587b6562e15a3d7d
mooonhok <1026413232@qq.com>
2017-10-26 14:39:11 +0800

    修改

---
+++ contactCompany.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/10/26
+ * Time: 14:37
+ */
+require 'Slim/Slim.php';
+require 'connect.php';
+
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+$app->get('/province',function ()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $database=localhost();
+    $selectStatement = $database->select()
+        ->from('province');
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetchAll();
+    echo  json_encode(array("result"=>"0","desc"=>"success","province"=>$data));
+});
+
+
+
+$app->run();
+
+function localhost(){
+    return connect();
+}
+?>
