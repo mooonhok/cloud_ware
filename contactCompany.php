483204bbc976dcc29cd0648964c4bb1eb511bc41
mooonhok <1026413232@qq.com>
2017-10-26 14:52:08 +0800

    修改

--- contactCompany.php
+++ contactCompany.php
@@ -11,15 +11,28 @@ require 'connect.php';
 
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
-$app->get('/province',function ()use($app){
+$app->post('/addCompany',function()use($app) {
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
-    $database=localhost();
-    $selectStatement = $database->select()
-        ->from('province');
-    $stmt = $selectStatement->execute();
-    $data = $stmt->fetchAll();
-    echo  json_encode(array("result"=>"0","desc"=>"success","province"=>$data));
+    $database = localhost();
+    $body = $app->request->getBody();
+    $body = json_decode($body);
+    $array=array();
+    foreach($body as $key=>$value){
+        $array[$key]=$value;
+    }
+
+    $insertStatement = $database->insert(array_keys($array))
+                ->into('contact_company')
+                ->values(array_values($array));
+    $insertId = $insertStatement->execute(false);
+    if($insertId){
+        echo json_encode(array("result" => "0", "desc" => "success"));
+    }else{
+        echo json_encode(array("result" => "1", "desc" => "信息提交不成功"));
+    }
+
+
 });
 
 
