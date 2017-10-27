f84ec213c7eaf51ed0720f752161de6a23c5ede2
mooonhok <1026413232@qq.com>
2017-10-27 15:28:14 +0800

    修改

--- background/javascript/pages/tenant.js
+++ background/javascript/pages/tenant.js
@@ -41,7 +41,7 @@ function loadtenants(adminid,page) {
                         //模拟渲染
                         document.getElementById('tb1').innerHTML = function(){
                             var arr = []
-                                ,thisData = msg.insurance_rechanges;
+                                ,thisData = msg.tenants;
                             layui.each(thisData, function(index, item){
                                     arr.push( '<tr><td>'+item.company+'</td><td>'+item.from_city+'</td><td>'+item.receive_city+'</td><td>'+item.tenant_num+'</td><td>'+item.customer.customer_name+'</td><td>'+item.sales_name+'</td><td>'+item.begin_time+'</td><td>'+item.end_date+'</td><td onclick="ins('+item.tenant_id + ')"><span style="color:blue; cursor:pointer;">扣费详情</span></td></tr>');
                             });
