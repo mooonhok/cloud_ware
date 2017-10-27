193f4b86bbb05075f755783693773c4c788cf32e
mooonhok <1026413232@qq.com>
2017-10-27 13:40:01 +0800

    修改

---
+++ background/javascript/pages/login.js
@@ -0,0 +1,41 @@
+$(function(){
+	$('#entry').click(function(){
+		if($('#adminName').val()==''){
+			$('.mask,.dialog').show();
+			$('.dialog .dialog-bd p').html('请输入管理员账号');
+		}else if($('#adminPwd').val()==''){
+			$('.mask,.dialog').show();
+			$('.dialog .dialog-bd p').html('请输入管理员密码');
+		}else if($(".yanzhengma").val().length<=0){
+			$('.mask,.dialog').show();
+			$('.dialog .dialog-bd p').html('请输入验证码');
+		}else if($(".yanzhengma").val().toUpperCase()!=code.toUpperCase()){
+			$('.mask,.dialog').show();
+			$('.dialog .dialog-bd p').html('验证码输入有误');
+			createCode();
+		}else{
+			$('.mask,.dialog').hide();
+			location.href='index1.html';
+			var name=$('#adminName').val();
+			var password=$('#adminPwd').val();
+			$.ajax({
+				url: "http://api.uminfo.cn/adminall.php/sign",
+				dataType: 'json',
+				type: 'post',
+				ContentType: "application/json;charset=utf-8",
+				data: JSON.stringify({
+					name:name,
+					password:password
+				}),
+				success: function(msg) {
+				   if(msg.result == 0) {
+					   window.location.href = "http://api.uminfo.cn/background/tenant.html;
+				  }
+				},
+				error: function(xhr) {
+					layer.msg("获取后台失败");
+				}
+			});
+		}
+	});
+});
