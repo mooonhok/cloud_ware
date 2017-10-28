696a7108da7f04b746e41a0822a967364cefeb92
mooonhok <1026413232@qq.com>
2017-10-28 16:39:40 +0800

    修改

--- tenant_insurancepolicy.php
+++ tenant_insurancepolicy.php
@@ -19,16 +19,17 @@ $app->post('/upload',function()use($app) {
     $database = localhost();
     $tenant_id = $app->request->get('tenant_id');
         $name31 = $_FILES["file1"]["name"];
-        $name3 = iconv("UTF-8", "gb2312", $name31);
+    $name3=substr(strrchr($name31, '.'), 1);
+//        $name3 = iconv("UTF-8", "gb2312", $name31);
         $shijian = time();
-        $name3 = $shijian . $name3;
+        $name3 = $shijian .'.'. $name3;
 //        move_uploaded_file($_FILES["file1"]["tmp_name"], "tenant/insurance/" . $name3);
     $url="/files/insurance_policy/";
     move_uploaded_file($_FILES["file1"]["tmp_name"], $url . $name3);
         if ($tenant_id != null || $tenant_id != '') {
             $insertStatement = $database->insert(array('tenant_id', 'tenant_insurancepolicy'))
                 ->into('tenant_insurancepolicy')
-                ->values(array($tenant_id,"http://files.uminfo.cn:8000/insurance_policy/".$shijian.$name31));
+                ->values(array($tenant_id,"http://files.uminfo.cn:8000/insurance_policy/".$name3));
             $insertId = $insertStatement->execute(false);
             echo json_encode(array("result" => "0", "desc" => "success"));
         } else {
