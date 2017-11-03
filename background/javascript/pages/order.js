347c45e51625e3c49bfd84a89d338cc96b8cfc3f
mooonhok <1026413232@qq.com>
2017-11-03 08:50:50 +0800

    修改

---
+++ background/javascript/pages/order.js
@@ -0,0 +1,116 @@
+$(function(){
+    var adminid=$.session.get('adminid');
+    var page = $.getUrlParam('page');
+    loadorders(adminid,page);
+});
+
+(function($) {
+    $.getUrlParam = function(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if(r != null) return decodeURI(r[2]);
+        return null;
+    }
+})(jQuery);
+
+function loadtenants(order_id,page) {
+    if(page==null){
+        page=1;
+    }
+    $.ajax({
+        url: "http://api.uminfo.cn/order.php/orders?order_id="+order_id+"&page="+page+"&per_page=10",
+        dataType: 'json',
+        type: 'get',
+        ContentType: "application/json;charset=utf-8",
+        data: JSON.stringify({}),
+        success: function(msg) {
+            console.log(msg)
+            $("#tb1").html("");
+            //调用分页
+            layui.use(['laypage', 'layer'], function(){
+                var laypage = layui.laypage;
+                laypage.render({
+                    elem: 'demo20'
+                    ,count:msg.count
+                    ,curr:page
+                    ,limit: 10
+                    ,jump: function(obj,first){
+                        if(!first){
+                            loadtenants(adminid,obj.curr);
+                        }
+                        //模拟渲染
+                        document.getElementById('tb1').innerHTML = function(){
+                            var arr = []
+                                ,thisData = msg.tenants;
+                            layui.each(thisData, function(index, item){
+                                arr.push( '<tr><td>'+item.company+'</td><td>'+item.from_city+'</td><td>'+item.receive_city+'</td><td>'+item.tenant_num+'</td><td>'+item.customer.customer_name+'</td><td>'+item.sales_name+'</td><td>'+item.begin_time+'</td><td>'+item.end_date+'</td><td onclick="tenant_xq('+item.tenant_id + ')"><span style="color:blue; cursor:pointer;">查看</span></td></tr>');
+                            });
+                            return arr.join('');
+                        }();
+                    }
+                });
+            });
+        },
+        error: function(xhr) {
+            alert("获取后台失败！");
+        }
+    });
+}
+
+
+function tenant_xq(id){
+    $(".tenant_tk").css("display","block");
+    $(".tenant_tk div input").val("");
+    $.ajax({
+        url: "http://api.uminfo.cn/tenant_nedb.php/getTenant1?tenant_id="+id,
+        dataType: 'json',
+        type: 'get',
+        ContentType: "application/json;charset=utf-8",
+        data: JSON.stringify({}),
+        success: function(msg) {
+            console.log(msg);
+            $("#tenant_id").val(msg.tenant.tenant_id);
+            $("#tenant_num").val(msg.tenant.tenant_num);
+            $("#app_id").val(msg.tenant.appid);
+            $("#secret").val(msg.tenant.secret);
+            $("#customer_name").val(msg.tenant.customer_name);
+            $("#customer_phone").val(msg.tenant.customer_phone);
+            $("#end_time").val(msg.tenant.end_time);
+            $("#address").val(msg.tenant.address);
+            $("#qq").val(msg.tenant.qq);
+            $("#email").val(msg.tenant.email);
+        },
+        error: function(xhr) {
+            alert("获取后台失败！");
+        }
+    });
+}
+
+function tenant_ensure(adminid){
+    $.ajax({
+        url: "http://api.uminfo.cn/adminall.php/uptenant",
+        dataType: 'json',
+        type: 'put',
+        ContentType: "application/json;charset=utf-8",
+        data: JSON.stringify({
+            tenant_id:$("#tenant_id").val(),
+            admin_id:adminid,
+            appid:$("#app_id").val(),
+            secret:$("#secret").val(),
+            customer_name:$("#customer_name").val(),
+            customer_phone:$("#customer_phone").val(),
+            address:$("#address").val(),
+            end_time:$("#end_time").val(),
+            qq:$("#qq").val(),
+            email:$("#email").val()
+        }),
+        success: function(msg) {
+            console.log(msg);
+            layer.msg(msg.desc);
+            $(".tenant_tk").css("display","none");
+        },
+        error: function(xhr) {
+            alert("获取后台失败！");
+        }
+    });
+}
