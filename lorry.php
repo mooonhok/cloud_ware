4ef5dd2298453174b42e85350e720b176ce25dbe
mooonhok <1026413232@qq.com>
2017-11-08 14:52:25 +0800

    修改

--- lorry.php
+++ lorry.php
@@ -308,7 +308,7 @@ $app->get('/lorrys_lorry_id',function()use($app){
     $selectStatement = $database->select()
         ->from('lorry')
         ->whereLike('lorry_id','%'.$lorry_id.'%')
-        ->havingCount('id');
+        ->havingCount('id','1','1');
     $stmt = $selectStatement->execute();
     $data0 = $stmt->fetch();
     $selectStatement = $database->select()
