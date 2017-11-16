8b4b361d056a42f8e99c5309e103874d0fa509fd
mooonhok <1026413232@qq.com>
2017-11-16 19:27:18 +0800

    修改

---
+++ inddd.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/16
+ * Time: 19:11
+ */
+$servername = "172.17.16.2";
+$username = "root";
+$password = "jsym_20170607";
+$dbname = "cloud_ware";
+$port=60026;
+// 创建连接
+$conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM staff";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // 输出数据
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . "<br>";
+    }
+} else {
+    echo "0 结果";
+}
+$conn->close();
+?>
