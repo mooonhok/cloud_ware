33c04492e83976b3368cebf895a49b2c5e3d4153
mooonhok <1026413232@qq.com>
2017-11-16 19:31:07 +0800

    修改

--- inddd.php
+++ inddd.php
@@ -5,13 +5,15 @@
  * Date: 2017/11/16
  * Time: 19:11
  */
+
 $servername = "172.17.16.2";
 $username = "root";
 $password = "jsym_20170607";
 $dbname = "cloud_ware";
 $port=60026;
+
 // 创建连接
-$conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
+$conn = new mysqli("{$servername}:{$port}",$servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
