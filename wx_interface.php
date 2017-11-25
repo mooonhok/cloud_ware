28bcd7448dfce4025a6a97d37b9a02d6938cb799
mooonhok <1026413232@qq.com>
2017-11-25 15:16:08 +0800

    修改

--- wx_interface.php
+++ wx_interface.php
@@ -9,16 +9,13 @@
  * wechat php test
  */
 
-require 'Slim/Slim.php';
-require 'connect.php';
 //define your token
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
-$a=$_GET['tenant_id'];
 if($_GET['echostr']){
     $wechatObj->valid();//如果发来了echostr则进行验证
 }else{
-    $wechatObj->responseMsg($a); //如果没有echostr，则返回消息
+    $wechatObj->responseMsg(); //如果没有echostr，则返回消息
 }
 
 class wechatCallbackapiTest
@@ -33,11 +30,11 @@ class wechatCallbackapiTest
         }
     }
 
-    public function responseMsg($a)
+    public function responseMsg()
     {
         //get post data, May be due to the different environments
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-       $data2=$this->getcompany($a);
+
         //extract post data
         if (!empty($postStr)) {
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
@@ -82,7 +79,7 @@ class wechatCallbackapiTest
             
             if ($ev == "subscribe") {
                 $resultStr = sprintf($newsTpl, $fromUsername, $toUsername, $time, 'news',
-                    $ArticleCount,'公司简介',$data2['company'],'http://files.uminfo.cn:8000/weixinguanggao/wsx.png',
+                    $ArticleCount,'公司简介','靖江万事鑫联运服务有限公司','http://files.uminfo.cn:8000/weixinguanggao/wsx.png',
                     '');
                 echo $resultStr;
             }
@@ -99,15 +96,7 @@ class wechatCallbackapiTest
             exit;
         }
     }
-    function getcompany($a){
-        $database=localhost();
-        $selectStatement = $database->select()
-            ->from('tenant')
-            ->where('tenant_id','=',$a);
-        $stmt = $selectStatement->execute();
-        $data1 = $stmt->fetch();
-        return $data1;
-    }
+
 
     private function checkSignature()
     {
@@ -128,8 +117,4 @@ class wechatCallbackapiTest
         }
     }
 }
-
-
-
-
 ?>
