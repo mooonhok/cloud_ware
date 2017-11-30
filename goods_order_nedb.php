6a62ef0fe00bea4012ccd6bfb38a7cb4b67e868b
mooonhok <1026413232@qq.com>
2017-11-30 14:30:23 +0800

    修改

--- goods_order_nedb.php
+++ goods_order_nedb.php
@@ -95,6 +95,7 @@ $app->get('/getGoodsOrders1',function()use($app){
             ->join('goods', 'goods.order_id', '=', 'orders.order_id', 'INNER')
             ->where('goods.tenant_id','=',$tenant_id)
             ->where('orders.tenant_id','=',$tenant_id)
+            ->whereNotIn('orders.order_status',array(-1,-2))
             ->where('orders.exist','=',0);
         $stmt = $selectStatement->execute();
         $data1 = $stmt->fetchAll();
@@ -982,6 +983,7 @@ $app->get('/limitGoodsOrders5',function()use($app){
                     ->join('goods', 'goods.order_id', '=', 'orders.order_id', 'INNER')
                     ->where('goods.tenant_id','=',$tenant_id)
                     ->where('orders.tenant_id','=',$tenant_id)
+                    ->whereNotIn('orders.order_status',array(-1,-2))
                     ->where('orders.exist','=',0)
                     ->orderBy('orders.order_id','DESC')
                     ->limit((int)$size,(int)$offset);
