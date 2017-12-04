20688d8d1603c65782e0b48917b0221c3ecd473a
GHOSTDO <2121066061@qq.com>
2017-12-04 15:38:39 +0800

    修改

--- weixin/my_consignment_note.php
+++ weixin/my_consignment_note.php
@@ -94,9 +94,10 @@ $signPackage = $jssdk->GetSignPackage();
         }
 
         function delet(id){
+
             layer.confirm('你确定删除该运单？', {
                 btn: ['确定','关闭'] //按钮
-            }, function(id){
+            }, function(){
                 alert(id)
                 $.ajax({
                     url: "http://api.uminfo.cn/wxmessage.php/wxmessage?messageid="+id,
