045bd416b770e87c6a95cea14d54fa48a0d5214c
mooonhok <1026413232@qq.com>
2017-12-05 22:13:53 +0800

    修改

--- guangwang/js/m_index.js
+++ guangwang/js/m_index.js
@@ -1,6 +1,14 @@
   var width=$(window).width();
   var height=$(window).height();
+                 function sport(){
+               $('.back_sport').animate({top:(height-100)+"px"},600);
+               $('.back_sport').animate({top:(height-110)+"px"},600);	
+               }
   $(document).ready(function(){
+  	        	    var width_sport=$('.back_sport').outerWidth();
+        	    $('.back_sport').css("left",(width-width_sport)/2);
+        	    $('.back_sport').css("top",(height-110)+"px");
+        	    var show=setInterval('sport()',1000);
   	$(".bg_img_a").css("width",width);
   	$(".bg_img_a").css("height",height);
   	  	$(".bg_img_b").css("width",width);
