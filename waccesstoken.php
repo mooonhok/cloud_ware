72a721a652c31d66ed7c5c9e4a98ee9c0b91e62c
GHOSTDO <2121066061@qq.com>
2017-12-26 15:40:38 +0800

    修改

--- waccesstoken.php
+++ waccesstoken.php
@@ -171,16 +171,15 @@ $app->post('/addpic',function()use($app){
                        $url = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token={$access_token}&type=image";
                        $ch1 = curl_init();
                        $timeout = 5;
-
                        $real_path="{$file_info['filename']}";
-                       $data= array("media"=>"@{$real_path}",'form-data'=>$file_info);
+                       $data6= array("media"=>"@{$real_path}",'form-data'=>$file_info);
                        curl_setopt($ch1, CURLOPT_URL, $url);
                        curl_setopt($ch1, CURLOPT_POST, 1);
                        curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
                        curl_setopt($ch1, CURLOPT_CONNECTTIMEOUT, $timeout);
                        curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
                        curl_setopt($ch1, CURLOPT_SSL_VERIFYHOST, false);
-                       curl_setopt($ch1, CURLOPT_POSTFIELDS, $data);
+                       curl_setopt($ch1, CURLOPT_POSTFIELDS, $data6);
                        $result = curl_exec($ch1);
                        curl_close($ch1);
                        if (curl_errno() == 0) {
