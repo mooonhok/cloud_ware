e811b338b3b0f3580b0fa3487bdffcf1df67fdd5
GHOSTDO <2121066061@qq.com>
2018-01-04 10:14:01 +0800

    修改

---
+++ msend.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/4
+ * Time: 10:02
+ */
+require 'Slim/Slim.php';
+require 'connect.php';
+use Slim\PDO\Database;
+
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+require_once 'ChuanglanSmsHelper/ChuanglanSmsApi.php';
+$clapi  = new ChuanglanSmsApi();
+$app->post('sendm',function()use($app,$clapi){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $body = $app->request->getBody();
+    $body=json_decode($body);
+    $phone=$body->tel;
+    if($phone!=null||$phone!=""){
+        $code = mt_rand(100000,999999);
+        $result = $clapi->sendSMS($phone, '【江苏酉铭】您好，您的验证码是'. $code);
+        if(!is_null(json_decode($result))){
+            $output=json_decode($result,true);
+            if(isset($output['code'])  && $output['code']=='0'){
+                echo json_encode(array("result" => "0", "desc" =>"短信发送成功","code"=>$code)) ;
+            }else{
+                echo json_encode(array("result" => "3", "desc" => $output['errorMsg']));
+            }
+        }else{
+            echo json_encode(array("result" => "2", "desc" => "短信缺少内容"));
+        }
+    }else{
+        echo json_encode(array("result" => "1", "desc" => "缺少电话号码"));
+    }
+});
+
+$app->run();
+
+function localhost(){
+    return connect();
+}
+?>
