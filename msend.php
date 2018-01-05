ace313ed8ed1a11294a307abb8e18e2741e84564
GHOSTDO <2121066061@qq.com>
2018-01-05 15:32:21 +0800

    修改

--- msend.php
+++ msend.php
@@ -65,8 +65,8 @@ $app->post("/sendtwo",function()use($app,$clapi){
                 $title=$data['jcompany'];
                 if($orderid!=null||$orderid!=""){
                     if($name!=null||$name!=""){
-                        if($address1!=null||$address1!=""){
-                            if($address2!=null||$address2!=""){
+
+
                                 $selectStatement = $database->select()
                                     ->from('customer')
                                     ->where('customer_id','=',$data['contact_id'])
@@ -108,12 +108,7 @@ $app->post("/sendtwo",function()use($app,$clapi){
                                     }else{
                                         echo json_encode(array("result" => "8", "desc" => "发送失败"));
                                     }
-                            }else{
-                                echo json_encode(array("result" => "7", "desc" => "缺少到达城市"));
-                            }
-                        }else{
-                            echo json_encode(array("result" => "7", "desc" => "缺少出发城市"));
-                        }
+
                     }else{
                         echo json_encode(array("result" => "6", "desc" => "缺少短信接收人姓名"));
                     }
@@ -134,70 +129,6 @@ $app->post("/sendtwo",function()use($app,$clapi){
     }
 });
 
-
-//到达单子短信通知货主
-$app->post("/schedules_sign",function()use($app,$clapi){
-    $app->response->headers->set('Access-Control-Allow-Origin','*');
-    $app->response->headers->set('Content-Type','application/json');
-    $body = $app->request->getBody();
-    $body=json_decode($body);
-    $database=localhost();
-    $scheduling_id=$body->scheduling_id;
-    if($scheduling_id!=null||$scheduling_id!=""){
-        $selectStatement = $database->select()
-            ->from('schedule_order')
-            ->where('schedule_order.schedule_id','=',$scheduling_id)
-            ->where('schedule_order.exist','=',0);
-        $stmt = $selectStatement->execute();
-        $dataa= $stmt->fetch();
-        $selectStatement = $database->select()
-            ->from('schedule_order')
-            ->join('scheduling','scheduling.schedule_id','=','scheduling.scheduling_id','INNER')
-            ->join('tenant','tenant.tenant_id','=','schedule_order.tenant_id','INNER')
-            ->join('customer','customer.customer_id','=','scheduling.receiver_id','INNER')
-            ->where('customer.tenant_id','=',$dataa['tenant_id'])
-            ->where('scheduling.tenant_id','=',$dataa['tenant_id'])
-            ->where('schedule_order.tenant_id','=',$dataa['tenant_id'])
-            ->where('schedule_order.schedule_id','=',$scheduling_id)
-            ->where('schedule_order.exist','=',0)
-            ->where('scheduling.exist','=',0);
-        $stmt = $selectStatement->execute();
-        $data1= $stmt->fetchAll();
-        for($i=0;$i<count($data1);$i++){
-            $title=$data1[$i]["company"];
-            $orderid=$data1[$i]["order_id"];
-            $customer_name=$data1[$i]["customer_name"];
-            $customer_phone=$data1[$i]["customer_phone"];
-            $selectStatement = $database->select()
-                ->from('orders')
-                ->join('customer','customer.customer_id','=','orders.receiver_id','INNER')
-                ->where('orders.order_id','=',$data1[$i]['order_id'])
-                ->where('orders.exist','=',0)
-                ->where('customer.tenant_id','=',$data1[$i]['tenant_id'])
-                ->where('orders.tenant_id','=',$data1[$i]['tenant_id']);
-            $stmt = $selectStatement->execute();
-            $data2= $stmt->fetch();
-            $msg = '【'.$title.'】{$var}，您好！您托运的运单号为'.$orderid.'的货物已被'.$customer_name.'签收，请及时验收或拨打'.$customer_phone.'确认。';
-            $phone=$data2['customer_phone'];
-            $name=$data2['customer_name'];
-//            $params = $phone.','.$name;
-//            $result = $clapi->sendVariableSMS($msg, $params);
-//            if(!is_null(json_decode($result))){
-//                $output=json_decode($result,true);
-//                if(isset($output['code'])  && $output['code']=='0'){
-//                    echo json_encode(array("result" => "0", "desc" => '发送成功'));
-//                }else{
-//                    echo  json_encode(array("result" => "3", "desc" => $output['errorMsg'].$title));
-//                }
-//            }else{
-//                echo json_encode(array("result" => "2", "desc" => "发送失败"));
-//            }
-        }
-        echo json_encode(array("result" => "0", "desc" => "success"));
-        }else{
-            echo json_encode(array("result" => "1", "desc" => "缺少清单号"));
-        }
-});
 
 
 
