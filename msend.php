a547da037f5317f9b48f0c91e956fff2bb111584
GHOSTDO <2121066061@qq.com>
2018-01-05 09:42:10 +0800

    修改

--- msend.php
+++ msend.php
@@ -54,15 +54,15 @@ $app->post("/sendtwo",function()use($app,$clapi){
     $address2=$body->tcity;
     if($phone!=null||$phone!=""){
         if($type==0){
-            $msg = '【'.$title.'】{$var},您好！,您托运的运单号为'.$orderid.'的货物已从'.$address1.'发往'.$address2.'。微信关注'
+            $msg = '【'.$title.'】{$var}，您好！您托运的运单号为'.$orderid.'的货物已从'.$address1.'发往'.$address2.'。微信关注'
                 .$title.'公众号查询运单详情';
         }else if($type==1){
-            $msg = '【'.$title.'】{$var},您好！,您即将签收的运单号为'.$orderid.'的货物已从'.$address1.'发往'.$address2.'。微信关注'
+            $msg = '【'.$title.'】{$var}，您好！您即将签收的运单号为'.$orderid.'的货物已从'.$address1.'发往'.$address2.'。微信关注'
                 .$title.'公众号查询运单详情';
          }else if($type==2){
-            $msg = '【'.$title.'】{$var},您好！,您即将签收的运单号为'.$orderid.'的货物已到达'.$address1.'中转。';
+            $msg = '【'.$title.'】{$var}，您好！您即将签收的运单号为'.$orderid.'的货物已到达'.$address1.'中转。';
         }else if($type==3){
-            $msg = '【'.$title.'】{$var},您好！,您即将签收的运单号为'.$orderid.'的货物已到达'.$address1.'的'.$title.',请及时验收或拨打'.$phone1.'修改提货方式';
+            $msg = '【'.$title.'】{$var}，您好！您即将签收的运单号为'.$orderid.'的货物已到达'.$address1.'的'.$title.',请及时验收或拨打'.$phone1.'修改提货方式。';
         }
         $params = $phone.','.$name;
         $result = $clapi->sendVariableSMS($msg, $params);
