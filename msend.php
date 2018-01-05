e13947d77baca457adccc3807966990003441668
GHOSTDO <2121066061@qq.com>
2018-01-05 17:46:57 +0800

    修改

--- msend.php
+++ msend.php
@@ -148,7 +148,8 @@ $app->post("/schedules_sign",function()use($app,$clapi){
         $stmt = $selectStatement->execute();
         $data1= $stmt->fetchAll();
         for($i=0;$i<count($data1);$i++){
-            $title=$data1[$i]["company"];
+//            $title=$data1[$i]["jcompany"];
+            $title='靖江万事鑫';
             $orderid=$data1[$i]["order_id"];
             $customer_name=$data1[$i]["customer_name"];
             $customer_phone=$data1[$i]["customer_phone"];
@@ -161,29 +162,46 @@ $app->post("/schedules_sign",function()use($app,$clapi){
                 ->where('orders.tenant_id','=',$data1[$i]['tenant_id']);
             $stmt = $selectStatement->execute();
             $data2= $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('city')
+                ->where('id', '=', $data1[$i]['send_city_id']);
+            $stmt = $selectStatement->execute();
+            $data3 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('city')
+                ->where('id', '=', $data1[$i]['receive_city_id']);
+            $stmt = $selectStatement->execute();
+            $data4 = $stmt->fetch();
             $msg = '【'.$title.'】您好！您托运的运单号为'.$orderid.'的货物已被'.$customer_name.'签收，联系电话：'.$customer_phone.'';
             $phone=$data2['customer_phone'];
 //            $name=$data2['customer_name'];
-//            $params = $phone;
-//            $result = $clapi->sendVariableSMS($msg, $params);
-//            if(!is_null(json_decode($result))){
-//                $output=json_decode($result,true);
-//                if(isset($output['code'])  && $output['code']=='0'){
-//                    echo json_encode(array("result" => "0", "desc" => '发送成功'));
-//                }else{
-//                    echo  json_encode(array("result" => "3", "desc" => $output['errorMsg'].$title));
-//                }
-//            }else{
-//                echo json_encode(array("result" => "2", "desc" => "发送失败"));
-//            }
+            $params = $phone;
+            $result = $clapi->sendVariableSMS($msg, $params);
+            if(!is_null(json_decode($result))){
+                $output=json_decode($result,true);
+                if(isset($output['code'])  && $output['code']=='0'){
+                    $arrays1['note_remain']=(int)$data1[$i]['note_remain']-1;
+                    $updateStatement = $database->update($arrays1)
+                        ->table('tenant')
+                        ->where('tenant_id', '=', $data1[$i]['tenant_id']);
+                    $affectedRows = $updateStatement->execute();
+                    $insertStatement = $database->insert(array('tenant_id','order_id','fcity','tcity','phone','type','exist'))
+                        ->into('note')
+                        ->values(array($data1[$i]['tenant_id'],$orderid,$data3['name'],$data4['name'],$phone,4,0));
+                    $insertId = $insertStatement->execute(false);
+                    echo json_encode(array("result" => "0", "desc" => '发送成功'));
+                }else{
+                    echo  json_encode(array("result" => "3", "desc" => $output['errorMsg'].$title));
+                }
+            }else{
+                echo json_encode(array("result" => "2", "desc" => "发送失败"));
+            }
         }
         echo json_encode(array("result" => "4", "desc" => "success"));
     }else{
         echo json_encode(array("result" => "1", "desc" => "缺少清单号"));
     }
 });
-
-
 $app->run();
 
 function localhost(){
