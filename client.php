3492d89ca94cc1139f8747ed00c72d9ee0b5291c
mooonhok <1026413232@qq.com>
2018-01-12 16:12:39 +0800

    修改

--- client.php
+++ client.php
@@ -31,6 +31,37 @@ $app->get('/getLatest',function()use($app){
     }
 });
 
+$app->post('/client_version',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $client_version = $app->request->params('client_version');
+
+    $version_asar = $_FILES["version_asar"]["name"];
+    $i=0;
+    if($version_asar){
+        $new_file = "/files/client/" . $client_version . "/";
+        if (!file_exists($new_file)) {
+            mkdir($new_file, 0700);
+        }
+        move_uploaded_file($_FILES["version_asar"]["tmp_name"], $new_file . $version_asar);
+        $i++;
+    }
+    $version_json = $_FILES["version_json"]["name"];
+    if($version_json){
+        $new_file = "/files/client/" . $client_version . "/";
+        if (!file_exists($new_file)) {
+            mkdir($new_file, 0700);
+        }
+        move_uploaded_file($_FILES["version_json"]["tmp_name"], $new_file . $version_json);
+        $i++;
+    }
+    if($i==2){
+        $app->redirect('http://www.uminfo.cn');
+    }
+
+});
+
+
 $app->run();
 function localhost(){
     return connect();
