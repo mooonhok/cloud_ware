f7664c3d753225037fa24dbdcf3fb2eb6228a316
GHOSTDO <2121066061@qq.com>
2018-01-16 17:53:28 +0800

    修改
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- ztest.php
+++ ztest.php
@@ -1,4 +1,9 @@
 <?php
-
-echo phpinfo();
+$word = new COM("word.application") or die("Can’t start Word!");
+echo "Loading Word, v. {$word->Version}";
+$word->Visible = 0;
+$word->Documents->open(dirname(__FILE__)."/ztest.doc");
+$test= $word->ActiveDocument->content->Text;
+echo $test;
+//echo phpinfo();
 ?>
