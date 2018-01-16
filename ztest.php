dd6dffc4224487177b943184cfeb619b97d5d111
GHOSTDO <2121066061@qq.com>
2018-01-16 17:56:04 +0800

    修改

--- ztest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$word = new COM("word.application") or die("Can’t start Word!");
-echo "Loading Word, v. {$word->Version}";
-$word->Visible = 0;
-$word->Documents->open(dirname(__FILE__)."/ztest.doc");
-$test= $word->ActiveDocument->content->Text;
-echo $test;
-//echo phpinfo();
-?>
