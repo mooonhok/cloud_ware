a7efbacc07880e538736f72c7f5b2f75679653fb
mooonhok <1026413232@qq.com>
2018-01-23 16:40:20 +0800

    修改

--- background/javascript/pages/lorry.js
+++ background/javascript/pages/lorry.js
@@ -154,6 +154,42 @@ function lorry_xq(id){
         }
     });
     $("#order_sure").on("click",function(){
-
+        var phone=$("#phone").val();
+        var name=$("#name").val();
+        var id_number=$("#id_card").val();
+        var plate_number=$("#plate_number").val();
+        var lorry_length_name=$("#lorry_length").val();
+        var lorry_type_name=$("#lorry_type").val();
+        var lorry_load_name=$("#lorry_weight").val();
+        // $("#id_z").attr("src",msg.lorrys.identity_card_z);
+        // $("#id_f").attr("src",msg.lorrys.identity_card_f);
+        // $("#j_z").attr("src",msg.lorrys.driver_license_fp);
+        // $("#j_f").attr("src",msg.lorrys.driver_license_tp);
+        // $("#x_z").attr("src",msg.lorrys.driving_license_fp);
+        // $("#x_f").attr("src",msg.lorrys.driving_license_tp);
+        var file1=$(this).parent().prev().children('#id_z').files[0];
+        console.log(file1);
+        // var file2=$(this).parent().prev().children('.afile');
+//         if(file1!=null&&tenant_id!=null){
+//             var formdata = new FormData();
+//             formdata.append('file1', file1);
+//             var xhr = new XMLHttpRequest();
+//             xhr.open("POST" ,"http://api.uminfo.cn/message.php/upload?tenant_id="+tenant_id , true);
+//             xhr.send(formdata);
+// //                      xhr.upload.onprogress = progressFunction;
+//             xhr.onload = function(e) {
+//                 if (this.status == 200) {
+//                     $("#ImgZoomInBG").hide();
+//                     $("#ImgZoomInImage").hide();
+//                     layer.msg('上传成功');
+//                     file2.val("");
+//                 };
+//             };
+//             return;
+//         }else{
+//             $("#ImgZoomInImage").css('display','none');
+//             $("#ImgZoomInBG").css('display','none');
+//             layer.msg('文件信息不全');
+//         }
     });
 }
