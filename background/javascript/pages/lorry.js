b1dcf7f286e8841f05896981a8faa879fd891838
mooonhok <1026413232@qq.com>
2018-01-23 17:37:23 +0800

    修改

--- background/javascript/pages/lorry.js
+++ background/javascript/pages/lorry.js
@@ -167,7 +167,10 @@ function lorry_xq(id){
         // $("#j_f").attr("src",msg.lorrys.driver_license_tp);
         // $("#x_z").attr("src",msg.lorrys.driving_license_fp);
         // $("#x_f").attr("src",msg.lorrys.driving_license_tp);
-        var file1=$('#id_z')[0];
+        var file1=$('.id_z')[0].files[0];
+        var reader = new FileReader();
+        var cc=reader.readAsDataURL(File);
+        $('#id_z').attr('src',cc);
         console.log(file1);
         // var file2=$(this).parent().prev().children('.afile');
 //         if(file1!=null&&tenant_id!=null){
