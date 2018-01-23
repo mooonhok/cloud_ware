cf0d4fed3aa649015db5c0ba85adfbff94d85f05
mooonhok <1026413232@qq.com>
2018-01-23 08:54:14 +0800

    修改

--- weixin/my_consignment_note.php
+++ weixin/my_consignment_note.php
@@ -268,11 +268,11 @@ $signPackage = $jssdk->GetSignPackage();
 				for(var i = 0; i < msg.orders.length; i++) {
 					var a="<div class='piaoyi_a' id='"+msg.orders[i].order_idd+"'>" +
                         "<div class='piaoyi_b'><div class='xian'></div><div class='yundan'><div class='yundan_1'><p>运单号:<span>"
-					+msg.orders[i].order_id+"</span></p><p>订单价格:<span>"
-					+msg.orders[i].order_cost+"</span></p></div><div class='yundan_2'><div class='yundan_2_1'><div class='city_1'>"
-					+msg.orders[i].sendcity+"</div><div class='name_1'>"
+					+msg.orders[i].order_id+"</span></p><p>运费:<span>"
+					+msg.orders[i].order_cost+"元</span></p></div><div class='yundan_2'><div class='yundan_2_1'><div class='city_1'>"
+					+msg.orders[i].sendcity+"</div><div class='name_1'>发:"
 					+msg.orders[i].sendname+"</div></div><div class='yundan_2_2'><p class='sta'><img src='images/to.png'></p></div><div class='yundan_2_1'><div class='city_2'>"
-					+msg.orders[i].acceptcity+"</div><div class='name_2'>"
+					+msg.orders[i].acceptcity+"</div><div class='name_2'>收:"
 					+msg.orders[i].acceptname+"</div></div></div></div>" +
                         "<div class='yundan_4'><div class='yundan_4_1' id='"+"c"+msg.orders[i].order_idd+"'>删除</div></div><div class='yundan_3'><div class='yundan_3_1'>"
 					+msg.orders[i].status+"</div></div><div class='xian'></div><div class='kongbai'></div>";
@@ -317,11 +317,11 @@ $signPackage = $jssdk->GetSignPackage();
 				for(var i = 0; i < msg.orders.length; i++) {
 					var a="<div class='piaoyi_a' id='"+msg.orders[i].order_idd+"'>" +
                         "<div class='piaoyi_b'><div class='xian'></div><div class='yundan'><div class='yundan_1'><p>运单号:<span>"
-					+msg.orders[i].order_id+"</span></p><p>订单价格:<span>"
-					+msg.orders[i].order_cost+"</span></p></div><div class='yundan_2'><div class='yundan_2_1'><div class='city_1'>"
-					+msg.orders[i].sendcity+"</div><div class='name_1'>"
+					+msg.orders[i].order_id+"</span></p><p>运费:<span>"
+					+msg.orders[i].order_cost+"元</span></p></div><div class='yundan_2'><div class='yundan_2_1'><div class='city_1'>"
+					+msg.orders[i].sendcity+"</div><div class='name_1'>发:"
 					+msg.orders[i].sendname+"</div></div><div class='yundan_2_2'><p class='sta'><img src='images/accept.png'></p></div><div class='yundan_2_1'><div class='city_2'>"
-					+msg.orders[i].acceptcity+"</div><div class='name_2'>"
+					+msg.orders[i].acceptcity+"</div><div class='name_2'>收:"
 					+msg.orders[i].acceptname+"</div></div></div></div>" +
                         "<div class='yundan_4'><div class='yundan_4_1' id='"+"d"+msg.orders[i].order_idd+"'>删除</div></div><div class='yundan_3'><div class='yundan_3_1'>"
 					+msg.orders[i].status+"</div></div><div class='xian'></div><div class='kongbai'></div>";
