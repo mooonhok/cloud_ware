d57f737ac89857a9da72a364ccc8a604035ee0e4
mooonhok <1026413232@qq.com>
2018-01-24 17:31:52 +0800

    修改

--- tenant_nedb.php
+++ tenant_nedb.php
@@ -56,6 +56,26 @@ $app->get('/getTenant1',function()use($app){
 
 });
 
+$app->put('/alterTenant1',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $tenant_id = $app->request->headers->get("tenant-id");
+    $database=localhost();
+    $body=$app->request->getBody();
+    $body=json_decode($body);
+    $is_join=$body->is_join;
+    if($tenant_id!=null||$tenant_id!=''){
+        $updateStatement = $database->update(array('is_join'=>$is_join))
+            ->table('tenant')
+            ->where('exist','=',0)
+            ->where('tenant_id','=',$tenant_id);
+        $affectedRows = $updateStatement->execute();
+        echo  json_encode(array("result"=>"0","desc"=>"sucess"));
+    }else{
+        echo  json_encode(array("result"=>"1","desc"=>"租户id不存在"));
+    }
+});
+
 $app->run();
 
 function localhost(){
