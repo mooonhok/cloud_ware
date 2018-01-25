bdaf445d27f4ef15e0a6254dcc0307996a4e79dc
GHOSTDO <2121066061@qq.com>
2018-01-25 14:11:29 +0800

    修改
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- tenantsback/javascript/pages/arg.js
+++ tenantsback/javascript/pages/arg.js
@@ -54,7 +54,7 @@ function loader(agr_id) {
               $("#do_comment").html(msg.agree.agreement_require);
               $(".do_schedules").append(msg.agree.schedules)
               $(".contract_time").html(msg.agree.agreement_time);
-              $(".sure_img").attr("src",msg.agree.sign_img);
+              $("#pic").attr('src', msg.customer.pic);
 			} else {
 				alert(msg.desc);
 			}
