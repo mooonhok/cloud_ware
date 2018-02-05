7ad69cf1e746348b78bd5c2ebef99f0611878751
GHOSTDO <2121066061@qq.com>
2018-02-05 11:54:31 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- sales.php
+++ sales.php
@@ -38,15 +38,19 @@ $app->get('/usersign',function ()use($app){
              $stmt=$selectStaement->execute();
              $data2=$stmt->fetch();
              if($data2!=null){
-                 echo json_encode(array('result'=>'0','desc'=>'登录成功','user'=>$data2));
+                 echo $_GET['callback']."(".json_encode(array('result'=>'0','desc'=>'登录成功','user'=>$data2)).")";
+//                 echo json_encode(array('result'=>'0','desc'=>'登录成功','user'=>$data2));
              }else{
-                 echo json_encode(array('result'=>'3','desc'=>'密码错误','user'=>''));
+                 echo $_GET['callback']."(".json_encode(array('result'=>'3','desc'=>'密码错误','user'=>'')).")";
+//                 echo json_encode(array('result'=>'3','desc'=>'密码错误','user'=>''));
              }
          }else{
-             echo json_encode(array('result'=>'2','desc'=>'用户不存在','user'=>''));
+             echo $_GET['callback']."(".json_encode(array('result'=>'2','desc'=>'用户不存在','user'=>'')).")";
+//             echo json_encode(array('result'=>'2','desc'=>'用户不存在','user'=>''));
          }
     }else{
-        echo json_encode(array('result'=>'1','desc'=>'用户名为空','user'=>''));
+        echo $_GET['callback']."(".json_encode(array('result'=>'1','desc'=>'用户名为空','user'=>'')).")";
+//        echo json_encode(array('result'=>'1','desc'=>'用户名为空','user'=>''));
     }
 
 });
