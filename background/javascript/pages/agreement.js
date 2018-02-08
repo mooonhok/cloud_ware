5d625cd09faa5a3cedd4cdc510064e5fd6ba63b6
mooonhok <1026413232@qq.com>
2018-02-08 15:20:56 +0800

    修改
    Signed-off-by: mooonhok <1026413232@qq.com>

--- background/javascript/pages/agreement.js
+++ background/javascript/pages/agreement.js
@@ -62,21 +62,21 @@ function loadagreements(agreement_id,page,company) {
                             var arr = []
                                 ,thisData = msg.agreements;
                             layui.each(thisData, function(index, item){
-                                var info='-';
-                                if(item.scheduling_status==1){
-                                    info='等待装车';
-                                }else if(item.scheduling_status==2){
-                                    info='装车完成';
-                                }else if(item.scheduling_status==3){
-                                    info='司机确认';
-                                }else if(item.scheduling_status==4){
-                                    info='在途';
-                                }else if(item.scheduling_status==5){
-                                    info='到达';
-                                }else if(item.scheduling_status==6){
-                                    info='取消';
-                                }
-                                arr.push( '<tr><td>'+item.scheduling_id+'</td><td>'+item.tenant.company+'</td><td>'+item.tenant_suoshu.name+'</td><td>'+item.send_city.name+'</td><td>'+item.receive_city.name+'</td><td>'+item.receiver.customer_name+'</td><td>'+item.scheduling_datetime+'</td><td>'+info+'</td><td onclick="scheduling_xq('+(item.scheduling_id+'') +')"><span style="color:blue; cursor:pointer;">查看</span></td></tr>');
+                                // var info='-';
+                                // if(item.scheduling_status==1){
+                                //     info='等待装车';
+                                // }else if(item.scheduling_status==2){
+                                //     info='装车完成';
+                                // }else if(item.scheduling_status==3){
+                                //     info='司机确认';
+                                // }else if(item.scheduling_status==4){
+                                //     info='在途';
+                                // }else if(item.scheduling_status==5){
+                                //     info='到达';
+                                // }else if(item.scheduling_status==6){
+                                //     info='取消';
+                                // }
+                                arr.push( '<tr><td>'+item.agreement_id+'</td><td>'+item.tenant.company+'</td><td>'+item.lorry.plate_number+'</td><td>'+item.lorry.driver_name+'</td><td>'+item.lorry.driver_phone+'</td><td>'+item.rcity+'</td><td>'+item.agreement_time+'</td><td>'+item.agreement_require+'</td><td onclick="scheduling_xq('+(item.agreement_id+'') +')"><span style="color:blue; cursor:pointer;">查看</span></td></tr>');
                             });
                             return arr.join('');
                         }();
