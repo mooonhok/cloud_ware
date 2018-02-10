4bdb2bbdceb03d04acb6121a6f31587004975a19
GHOSTDO <2121066061@qq.com>
2018-02-10 12:48:13 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- background/javascript/pages/mini.js
+++ background/javascript/pages/mini.js
@@ -1,9 +1,78 @@
 $(function() {
 	var adminid = $.session.get('adminid');
 	var page = null;
-
+$.ajax({
+	url: "http://api.uminfo.cn/city.php/province",
+	dataType: 'json',
+	type: 'get',
+	ContentType: "application/json;charset=utf-8",
+	data: JSON.stringify({}),
+	success: function(msg) {
+		for(var i = 0; i < msg.province.length; i++) {
+			$("#province1").append('<option value="' + msg.province[i].id + '">' + msg.province[i].name + '</option>');
+		}
+	},
+	error: function(e) {
+		alert("省份列表1信息出错!");
+	}
+});
+ $.ajax({
+	url: "http://api.uminfo.cn/city.php/province",
+	dataType: 'json',
+	type: 'get',
+	ContentType: "application/json;charset=utf-8",
+	data: JSON.stringify({}),
+	success: function(msg) {
+		for(var i = 0; i < msg.province.length; i++) {
+			$("#province2").append('<option value="' + msg.province[i].id + '">' + msg.province[i].name + '</option>');
+		}
+	},
+	error: function(e) {
+		alert("省份列表2信息出错!");
+	}
+});
 	loadminis(page);
 });
+
+function getPro1() {
+	$("#city1").empty();
+	var pid = $("#province1 option:selected").val();
+	$.ajax({
+		url: "http://api.uminfo.cn/city.php/city?pid=" + pid,
+		dataType: 'json',
+		type: 'get',
+		ContentType: "application/json;charset=utf-8",
+		data: JSON.stringify({}),
+		success: function(msg) {
+			for(var i = 0; i < msg.city.length; i++) {
+				$("#city1").append('<option value="' + msg.city[i].id + '">' + msg.city[i].name + '</option>');
+			}
+		},
+		error: function(e) {
+			$("#city1").append('<option value="">请选择市</option>');
+		}
+	});
+}
+
+function getPro2() {
+	$("#city2").empty();
+	var pid = $("#province2 option:selected").val();
+	$.ajax({
+		url: "http://api.uminfo.cn/city.php/city?pid=" + pid,
+		dataType: 'json',
+		type: 'get',
+		ContentType: "application/json;charset=utf-8",
+		data: JSON.stringify({}),
+		success: function(msg) {
+			for(var i = 0; i < msg.city.length; i++) {
+				$("#city2").append('<option value="' + msg.city[i].id + '">' + msg.city[i].name + '</option>');
+			}
+		},
+		error: function(e) {
+			$("#city2").append('<option value="">请选择市</option>');
+		}
+	});
+}
 
 function loadminis(page) {
 	if(page == null) {
@@ -52,7 +121,10 @@ function loadminis(page) {
 									'</td><td onclick="minishow(' +
 									item.id + ')"><span style="color:blue; cursor:pointer;">查看</span>' +
 									'</td><td onclick="routeshow(' +
-									item.id + ')"><span style="color:blue; cursor:pointer;">查看</span>' + '</tr>');
+									item.id + ')"><span style="color:blue; cursor:pointer;">查看</span>'+
+									'</td><td onclick="addroute(' +
+									item.id + ')"><span style="color:blue; cursor:pointer;">添加</span>'  
+									+ '</td></tr>');
 							});
 							return arr.join('');
 						}();
@@ -68,7 +140,6 @@ function loadminis(page) {
 }
 
 function minishow(id) {
-
 	var index = layer.open({
 		type: 1,
 		skin: 'layui-layer-rim', //加上边框
@@ -173,7 +244,7 @@ function routeshow(id) {
 			'<input type="text" id="id" style="display:none;"/>' +
 			'<input type="text" id="company" disabled="disabled"/>' +
 			'</div>' +
-			'<button id="route_sure">确定</button><button id="route_cancle">取消</button>' +
+			'<button id="route_cancle">关闭</button>' +
 			'</div>'
 	});
 	$("#route_cancle").on("click", function() {
@@ -202,5 +273,59 @@ function routeshow(id) {
 			alert("获取后台失败！");
 		}
 	});
-
 }
+
+function  addroute(id){
+	$(".tenant_tk2").css("display","block");
+	$("#cancle").on("click", function() {
+		$(".tenant_tk2").css("display","none");
+	});
+	$.ajax({
+		url: "http://api.uminfo.cn/mini.php/minibyid?tid=" + id,
+		dataType: 'json',
+		type: 'get',
+		ContentType: "application/json;charset=utf-8",
+		data: JSON.stringify({}),
+		success: function(msg) {
+			if(msg.result == 0) {
+				$("#id").val(msg.routes.id);
+				$("#name").val(msg.routes.name);
+			} else {
+				alert(msg.desc);
+			}
+		},
+		error: function(xhr) {
+			alert("获取后台失败！");
+		}
+	});
+     
+    $("#route_add").on("click",function(){
+        var fcity=$("#city1").val();
+	    var tcity=$("#city2").val();
+	    var id=$("#id").val();
+    	$.ajax({
+		url: "http://api.uminfo.cn/mini.php/addroute",
+		dataType: 'json',
+		type: 'post',
+		ContentType: "application/json;charset=utf-8",
+		data: JSON.stringify({
+			fcityid:fcity,
+			tcityid:tcity,
+			tid:id,
+		}),
+		success: function(msg) {
+			if(msg.result==0){
+				alert("添加成功");
+				$(".tenant_tk2").css("display","none");
+			}else{
+				alert(msg.desc);
+			}
+		},
+		error: function(xhr) {
+			alert("获取后台失败！");
+		}
+	  });
+    });
+		
+}
+
