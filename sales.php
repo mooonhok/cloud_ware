34260e2382c96f90b55fed192f22861561b64158
GHOSTDO <2121066061@qq.com>
2018-02-23 16:35:33 +0800

    修改
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- sales.php
+++ sales.php
@@ -52,8 +52,49 @@ $app->get('/usersign',function ()use($app){
         echo $_GET['callback']."(".json_encode(array('result'=>'1','desc'=>'用户名为空','user'=>'')).")";
 //        echo json_encode(array('result'=>'1','desc'=>'用户名为空','user'=>''));
     }
-
 });
+
+$app->get('/signtwo',function ()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $database=localhost();
+    $username = $app->request->get("username");
+    $password1=$app->request->get("password");
+    $str1=str_split($password1,3);
+    $password=null;
+    for ($x=0;$x<count($str1);$x++){
+        $password.=$str1[$x].$x;
+    }
+    if($username!=""||$username!=null){
+        $selectStaement=$database->select()
+            ->from('sales')
+            ->where('exist','=',0)
+            ->where('sales_id','=',$username);
+        $stmt=$selectStaement->execute();
+        $data=$stmt->fetch();
+        if ($data!=null){
+            $selectStaement=$database->select()
+                ->from('sales')
+                ->where('password','=',$password)
+                ->where('exist','=',0)
+                ->where('sales_id','=',$username);
+            $stmt=$selectStaement->execute();
+            $data2=$stmt->fetch();
+            if($data2!=null){
+                echo json_encode(array('result'=>'0','desc'=>'登录成功','user'=>$data2));
+            }else{
+                echo json_encode(array('result'=>'3','desc'=>'密码错误','user'=>''));
+            }
+        }else{
+            echo json_encode(array('result'=>'2','desc'=>'用户不存在','user'=>''));
+        }
+    }else{
+        echo json_encode(array('result'=>'1','desc'=>'用户名为空','user'=>''));
+    }
+});
+
+
+
 //统计公司总数
 $app->get('/alltenants',function()use($app) {
     $app->response->headers->set('Access-Control-Allow-Origin', '*');
