98623feb12cd02d672a5c668a560096d156c1ddb
GHOSTDO <2121066061@qq.com>
2018-02-28 10:17:09 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- statistic.php
+++ statistic.php
@@ -35,7 +35,32 @@ $app->get('/getStatistic0',function()use($app){
 });
 
 
-
+$app->get('/getStatistic1',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $tenant_id = $app->request->headers->get("tenant-id");
+    $database=localhost();
+    $customer_id=$app->request->get('customer_id');
+    $time1=$app->request->get('begintime');
+    $time2=$app->request->get('endtime');
+    if($tenant_id!=null||$tenant_id!=""){
+        if($customer_id!=null||$customer_id!=""){
+            $selectStatement = $database->select()
+                ->from('orders')
+                ->where('sender_id','=',$customer_id)
+                ->where('order_datetime1','>',$time1)
+                ->where('order_datetime1','<',$time2)
+                ->where('tenant_id','=',$tenant_id);
+            $stmt = $selectStatement->execute();
+            $data = $stmt->fetchAll();
+            echo  json_encode(array("result"=>"0","desc"=>"success","orders"=>$data));
+        }else{
+            echo  json_encode(array("result"=>"1","desc"=>"缺少客户id"));
+        }
+    }else{
+        echo  json_encode(array("result"=>"1","desc"=>"缺少租户id"));
+    }
+});
 
 
 
