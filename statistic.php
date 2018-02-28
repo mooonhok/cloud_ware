842b27c2e09780f8bf1ab3d54a350888317715f8
GHOSTDO <2121066061@qq.com>
2018-02-28 20:35:59 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- statistic.php
+++ statistic.php
@@ -444,9 +444,9 @@ $app->get('/getStatistic4',function()use($app){
                         }
                         $selectStatement = $database->select()
                             ->from('orders')
-                            ->where('id','<',$data1000['id'])
+                            ->where('id','>',$data1000['id'])
                             ->where('order_id', '=', $data300[$y]['order_id'])
-                            ->orderBy('id','DESC');
+                            ->orderBy('id');
                         $stmt = $selectStatement->execute();
                         $data1200 = $stmt->fetchAll();
                         if($data1200!=null){
