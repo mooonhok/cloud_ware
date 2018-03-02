696883d36d2c608cf5b1789cc4a3409b225d346c
GHOSTDO <2121066061@qq.com>
2018-03-02 13:51:52 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- weixin/send.php
+++ weixin/send.php
@@ -72,6 +72,7 @@ $signPackage = $jssdk->GetSignPackage();
 			success: function(msg) {
 			    for(var i=0;i<msg.tenants.length;i++){
 			    	$("#center").append("<div class='but' id='center"+i+"'>"+msg.tenants[i].jcompany+"<div>");
+			    	alert(msg.tenants[i].tenantimg);
 			    	$("#center"+i).css("background","url('"+msg.tenants[i].tenantimg+"')");
 			    	$("#center"+i).on("click",function(){
 			    		var a=$(this).attr('id').substring(6);
