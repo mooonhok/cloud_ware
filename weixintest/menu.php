20befbc6a31e274d5e5cea16d1547a4b80ba5b02
GHOSTDO <2121066061@qq.com>
2018-03-14 17:41:34 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- weixintest/menu.php
+++ weixintest/menu.php
@@ -1,4 +1,6 @@
 <?php
+//header("Access-Control-Allow-Origin:*");
+//header("Access-Control-Allow-Methods:GET,POST,PUT,DELETE,OPTIONS");
 header('Content-type:text/html;charset=utf-8');
 $str=$_SERVER["QUERY_STRING"];
 $arr=explode("=",$str);
@@ -7,17 +9,60 @@ $page=$arr[4];
 $appid=substr($arr[2],0,18);
 $secret=substr($arr[3],0,32);
 
-
-function getOpenID($appid,$appsecret,$code){
-$url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=". 
-$appsecret."&code=".$code."&grant_type=authorization_code";
-
-$weixin=file_get_contents($url);//通过code换取网页授权access_token
-$jsondecode=json_decode($weixin); //对JSON格式的字符串进行编码
-$array = get_object_vars($jsondecode);//转换成数组
-$openid = $array['openid'];//输出openid
-return $openid;
+if ($_COOKIE['openid'.$tenant_id] == null) {
+    if (!isset($_GET['code'])) {
+      //  $appid = 'wx81d659de6151801e';
+        $redirect_uri = urlencode('http://api.uminfor.cn/weixintest/menu.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret.'&page='.$page);
+        $scope = 'snsapi_base';
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
+        header('Location:' . $url);
+    } else {
+       // $appid = "wx81d659de6151801e";
+       // $secret = "a777207a723e6f5ce885687caa5198e3";
+        $code = $_GET["code"];
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $json_obj = json_decode($output, true);
+        // echo $json_obj['openid'];
+        setcookie('openid'.$tenant_id, $json_obj['openid']);
+         if ($page==7){
+            header('location:http://api.uminfor.cn/weixintest/build.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==6){
+            header('location:http://api.uminfor.cn/weixintest/my_consignment_note.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==5){
+            header('location:http://api.uminfor.cn/weixintest/register.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==4){
+            header('location:http://api.uminfor.cn/weixintest/c_tenant.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+       }else if($page==3){
+            header('location:http://api.uminfor.cn/weixintest/c_name.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==2){
+           header('location:http://api.uminfor.cn/weixintest/query.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==1){
+            header('location:http://api.uminfor.cn/weixintest/send.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }
+    }
+}else{
+        if ($page==7){
+            header('location:http://api.uminfor.cn/weixintest/build.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==6){
+            header('location:http://api.uminfor.cn/weixintest/my_consignment_note.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==5){
+            header('location:http://api.uminfor.cn/weixintest/register.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==4){
+            header('location:http://api.uminfor.cn/weixintest/c_tenant.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+       }else if($page==3){
+            header('location:http://api.uminfor.cn/weixintest/c_name.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==2){
+           header('location:http://api.uminfor.cn/weixintest/query.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }else if($page==1){
+            header('location:http://api.uminfor.cn/weixintest/send.php?tenant_id='.$tenant_id.'&appid='.$appid.'&secret='.$secret);
+        }
 }
 
-echo getOpenID($appid,$appsecret,$code);
 ?>
