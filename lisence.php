3bd415d839e83421ef0c4b3b5710581c5c9b3b74
GHOSTDO <2121066061@qq.com>
2018-03-15 13:16:12 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- lisence.php
+++ lisence.php
@@ -175,6 +175,25 @@ $app->get('/getRecords',function()use($app){
     echo json_encode(array("result" => "0", "desc" => "","lisence_record"=>$data));
 });
 
+
+$app->get('/limitRecords',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $database=localhost();
+    $id=$app->request->get('admin_id');
+    $size= $app->request->get('size');
+    $offset= $app->request->get('offset');
+    $selectStatement = $database->select()
+        ->from('lisence_record')
+        ->where('admin_id','=',$id)
+        ->limit((int)$size,(int)$offset);
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetchAll();
+    echo json_encode(array("result" => "0", "desc" => "","lisence_record"=>$data));
+});
+
+
+
 $app->post('/addRecord',function()use($app){
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
