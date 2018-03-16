c39d39bdb160c025ffb60ed574c6f555a77b0956
GHOSTDO <2121066061@qq.com>
2018-03-16 10:12:41 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- download.php
+++ download.php
@@ -24,6 +24,53 @@ $app->get('/getDownload',function()use($app){
 });
 
 
+$app->post('/addRecord',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $database=localhost();
+    $body=$app->request->getBody();
+    $body=json_decode($body);
+    $sales_id=$body->sales_id;
+    $type=$body->type;
+    $time=$body->time;
+    $ip=$body->ip;
+    $area=$body->area;
+    if($sales_id!=null||$sales_id!=""){
+        if($type!=null||$type!=""){
+            if($time!=null||$time!=""){
+                if($ip!=null||$ip!=""){
+                    if($area!=null||$area!=""){
+                        $selectStatement = $database->select()
+                            ->from('download_record');
+                        $stmt = $selectStatement->execute();
+                        $data = $stmt->fetchAll();
+                        $insertStatement = $database->insert(array('id','sales_id','type','time','ip','area'))
+                            ->into('download_record')
+                            ->values(array(count($data)+1,$sales_id,$type,$time,$ip,$area));
+                        $insertId = $insertStatement->execute(false);
+                        echo json_encode(array("result" => "0", "desc" => "添加成功"));
+                    }else{
+                        echo json_encode(array("result" => "5", "desc" => "缺少所属地区"));
+                    }
+                }else{
+                    echo json_encode(array("result" => "4", "desc" => "缺少ip"));
+                }
+            }else{
+                echo json_encode(array("result" => "3", "desc" => "缺少时间"));
+            }
+        }else{
+            echo json_encode(array("result" => "2", "desc" => "缺少下载类别"));
+        }
+    }else{
+        echo json_encode(array("result" => "1", "desc" => "缺少业务员id"));
+    }
+});
+
+
+
+
+
+
 $app->run();
 function localhost(){
     return connect();
