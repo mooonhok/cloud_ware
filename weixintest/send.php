3ba8dfcb954bf1be2747423b6a7d468e446e53b5
GHOSTDO <2121066061@qq.com>
2018-03-17 14:31:47 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- weixintest/send.php
+++ weixintest/send.php
@@ -12,7 +12,6 @@ $signPackage = $jssdk->GetSignPackage();
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<meta name="referrer" content="no-referrer"/>
 		<meta http-equiv="Access-Control-Allow-Origin" content="*">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
