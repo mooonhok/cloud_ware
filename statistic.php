22c44bba0c60c82c43e66dc1d8a7d3e2d983bb7c
GHOSTDO <2121066061@qq.com>
2018-03-17 17:05:10 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- statistic.php
+++ statistic.php
@@ -777,7 +777,7 @@ $app->get('/getStatistic6',function()use($app){
                         $data[$x]['tcity'] = $data7['name'];
                     }
                 }
-            echo  json_encode(array("result"=>"0","desc"=>"success","orders"=>$data,'length'=>$length));
+            echo  json_encode(array("result"=>"0","desc"=>"success","orders"=>$data,'length'=>$length[0],'count'=>count($length)));
             }else{
                 echo  json_encode(array("result"=>"2","desc"=>"缺少付款方式"));
             }
