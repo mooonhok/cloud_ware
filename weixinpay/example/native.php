09fb1be49624111a4930574800671afb72a20d35
mooonhok <1026413232@qq.com>
2018-04-04 16:07:12 +0800

    修改

--- weixinpay/example/native.php
+++ weixinpay/example/native.php
@@ -54,6 +54,60 @@ $url2 = $result["code_url"];
 	<br/><br/><br/>
 	<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式二</div><br/>
 	<img alt="模式二扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=<?php echo urlencode($url2);?>" style="width:150px;height:150px;"/>
-	
+    <div id="myDiv"></div><div id="timer">0</div>
 </body>
+
+     <script>
+    //设置每隔1000毫秒执行一次load() 方法
+       var myIntval=setInterval(function(){load()},1000);
+       function load(){
+              document.getElementById("timer").innerHTML=parseInt(document.getElementById("timer").innerHTML)+1;
+              var xmlhttp;
+               if (window.XMLHttpRequest){
+                        // code for IE7+, Firefox, Chrome, Opera, Safari
+                     xmlhttp=new XMLHttpRequest();
+                  }else{
+                       // code for IE6, IE5
+                       xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+                   }
+             xmlhttp.onreadystatechange=function(){
+                        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+                              trade_state=xmlhttp.responseText;
+                              if(trade_state=='SUCCESS'){
+                                    document.getElementById("myDiv").innerHTML='支付成功';
+                                   //alert(transaction_id);
+                                  //延迟3000毫秒执行tz() 方法
+                                      clearInterval(myIntval);
+                                  setTimeout("location.href='success.php'",3000);
+
+                               }else if(trade_state=='REFUND'){
+                                   document.getElementById("myDiv").innerHTML='转入退款';
+                                      clearInterval(myIntval);
+                               }else if(trade_state=='NOTPAY'){
+                                     document.getElementById("myDiv").innerHTML='请扫码支付';
+
+                                   }else if(trade_state=='CLOSED'){
+                                       document.getElementById("myDiv").innerHTML='已关闭';
+                                      clearInterval(myIntval);
+                                  }else if(trade_state=='REVOKED'){
+                                    document.getElementById("myDiv").innerHTML='已撤销';
+                                     clearInterval(myIntval);
+                                  }else if(trade_state=='USERPAYING'){
+                                       document.getElementById("myDiv").innerHTML='用户支付中';
+                                 }else if(trade_state=='PAYERROR'){
+                                       document.getElementById("myDiv").innerHTML='支付失败';
+                                    clearInterval(myIntval);
+                                  }
+
+                           }
+                  }
+              //orderquery.php 文件返回订单状态，通过订单状态确定支付状态
+              xmlhttp.open("POST","orderquery.php",false);
+                //下面这句话必须有
+               //把标签/值对添加到要发送的头文件。
+                xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                 xmlhttp.send("out_trade_no=<?php echo $num;?>");
+
+                }
+       </script>
 </html>
