013373a4984f6fbd2ffa7f4da08d962d0757b08f
mooonhok <1026413232@qq.com>
2018-04-10 14:49:00 +0800

    xg

--- ticket.php
+++ ticket.php
@@ -25,6 +25,19 @@ $app->get('/gettickets',function()use($app){
         echo  json_encode(array("result"=>"1","desc"=>"",'ticket'=>$data));
 });
 
+$app->get('/getticket',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $id=$app->request->get('id');
+    $database = localhost();
+    $selectStatement = $database->select()
+        ->from('ticket')
+        ->where('id','=',$id);
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetch();
+    echo  json_encode(array("result"=>"1","desc"=>"",'ticket'=>$data));
+});
+
 $app->run();
 
 function file_url(){
