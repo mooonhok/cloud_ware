28483dd43f8c3192c248b4a0b6145037ff061eac
GHOSTDO <2121066061@qq.com>
2018-04-11 15:26:32 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- ticket_lorry.php
+++ ticket_lorry.php
@@ -3,8 +3,9 @@
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2018/4/11
- * Time: 15:15
+ * Time: 15:21
  */
+
 require 'Slim/Slim.php';
 require 'connect.php';
 require 'files_url.php';
@@ -12,18 +13,19 @@ require 'files_url.php';
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 
-$app->get('/getTicketTenants0',function()use($app){
+$app->get('/getTicketLorrys0',function()use($app){
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
-    $tenant_id=$app->request->get('tenant_id');
+    $lorry_id=$app->request->get('lorry_id');
     $database = localhost();
-        $selectStatement = $database->select()
-            ->from('ticket_tenant')
-            ->where('tenant_id','=',$tenant_id);
-        $stmt = $selectStatement->execute();
-        $data2 = $stmt->fetchAll();
-    echo  json_encode(array("result"=>"0","desc"=>"",'ticket_tenants'=>$data2));
+    $selectStatement = $database->select()
+        ->from('ticket_lorry')
+        ->where('lorry_id','=',$lorry_id);
+    $stmt = $selectStatement->execute();
+    $data2 = $stmt->fetchAll();
+    echo  json_encode(array("result"=>"0","desc"=>"",'ticket_lorrys'=>$data2));
 });
+
 
 
 $app->run();
