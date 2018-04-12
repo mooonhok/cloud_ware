0d1780e3183e3c0b1d7b3a9086d70556072c987a
mooonhok <1026413232@qq.com>
2018-04-12 16:35:28 +0800

    修改

--- zhifu.php
+++ zhifu.php
@@ -5,9 +5,9 @@
  * Date: 2018/4/12
  * Time: 15:28
  */
-require "./weixinpay/example/lib/WxPay.Api.php";
-require "./weixinpay/example/WxPay.NativePay.php";
-require './weixinpay/example/log.php';
+require_once "weixinpay/example/lib/WxPay.Api.php";
+require_once "weixinpay/example/WxPay.NativePay.php";
+require_once 'weixinpay/example/log.php';
 require 'Slim/Slim.php';
 require 'connect.php';
 require 'files_url.php';
