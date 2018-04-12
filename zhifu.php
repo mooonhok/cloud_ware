e072eb784a46cbb6b3c74c3b5484b0591914d76f
mooonhok <1026413232@qq.com>
2018-04-12 16:48:14 +0800

    xg

--- zhifu.php
+++ zhifu.php
@@ -21,19 +21,19 @@ $app->get('/gettickets',function()use($app,$notify,$input){
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
 
-//    $input->SetBody("test");
-//    $input->SetAttach("test");
-//    $num=WxPayConfig::MCHID.date("YmdHis");
-//    $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-//    $input->SetTotal_fee("1");
-//    $input->SetTime_start(date("YmdHis"));
-//    $input->SetTime_expire(date("YmdHis", time() + 600));
-//    $input->SetGoods_tag("test");
-//    $input->SetNotify_url("http://api.uminfo.cn/weixinpay/example/notify.php");
-//    $input->SetTrade_type("NATIVE");
-//    $input->SetProduct_id("123456789");
-//    $result = $notify->GetPayUrl($input);
-//    $url2 = $result["code_url"];
+    $input->SetBody("test");
+    $input->SetAttach("test");
+    $num=WxPayConfig::MCHID.date("YmdHis");
+    $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+    $input->SetTotal_fee("1");
+    $input->SetTime_start(date("YmdHis"));
+    $input->SetTime_expire(date("YmdHis", time() + 600));
+    $input->SetGoods_tag("test");
+    $input->SetNotify_url("http://api.uminfo.cn/weixinpay/example/notify.php");
+    $input->SetTrade_type("NATIVE");
+    $input->SetProduct_id("123456789");
+    $result = $notify->GetPayUrl($input);
+    $url2 = $result["code_url"];
     echo  json_encode(array("result"=>"1","desc"=>"",'ticket'=>''));
 });
 
