90db9ae6e389b71db2b70b0a5837dfd12371aff4
GHOSTDO <2121066061@qq.com>
2018-04-13 11:55:13 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- ticket_tenant.php
+++ ticket_tenant.php
@@ -40,6 +40,55 @@ $app->get('/getTicketTenant0',function()use($app){
     echo  json_encode(array("result"=>"0","desc"=>"",'ticket_tenant'=>$data));
 });
 
+$app->post('/addTicketTenant',function()use($app) {
+    $app->response->headers->set('Access-Control-Allow-Origin', '*');
+    $app->response->headers->set('Content-Type', 'application/json');
+    $body = $app->request->getBody();
+    $file_url = file_url();
+    $body = json_decode($body);
+    $database = localhost();
+    $tenant_id = $app->request->params('tenant_id');
+    $company_id = $app->request->params('company_id');
+    $company_name = $app->request->params('name');
+    $business = $app->request->params('business');
+//    $business_img=$body->business_img;
+    if (isset($_FILES["business_img"]["name"])) {
+    $name = $_FILES["business_img"]["name"];
+    $name1 = iconv("UTF-8", "gb2312", $name);
+    $time1 = time();
+    $name1 = $time1 . $name1;
+    move_uploaded_file($_FILES["business_img"]["tmp_name"], '/files/business/' . $name1);
+    $business_img = $file_url . 'business/' . $time1 . $name . '';
+}
+    if($tenant_id!=null||$tenant_id!=null){
+        if($company_id!=null||$company_id!=""){
+            $selectStatement = $database->select()
+                ->from('ticket_tenant')
+                ->where('tenant_id','=',$tenant_id)
+                ->where('company_id','=',$company_id);
+            $stmt = $selectStatement->execute();
+            $data = $stmt->fetch();
+            if($data==null){
+                $selectStatement = $database->select()
+                    ->from('ticket_tenant');
+                $stmt = $selectStatement->execute();
+                $data2 = $stmt->fetchAll();
+                $insertStatement = $database->insert(array('id','tenant_id','company_id','name','business','business_img','is_check','commit_time'))
+                    ->into('ticket_tenant')
+                    ->values(array(count($data2),$tenant_id,$company_id,$company_name,$business,$business_img,0,date('Y-m-d H:i:s',time())));
+                $insertId = $insertStatement->execute(false);
+            }else{
+                echo  json_encode(array("result"=>"3","desc"=>"该纪录已存在"));
+            }
+        }else{
+            echo  json_encode(array("result"=>"2","desc"=>"缺少公司id"));
+        }
+    }else{
+        echo  json_encode(array("result"=>"1","desc"=>"缺少租户id"));
+    }
+});
+
+
 $app->run();
 
 function file_url(){
