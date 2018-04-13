682699e15863ee9e5351d364580ffd7a9f9c12e9
GHOSTDO <2121066061@qq.com>
2018-04-13 16:13:52 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- order_nedb.php
+++ order_nedb.php
@@ -545,6 +545,7 @@ $app->put('/alterOrder8',function()use($app){
     if($tenant_id!=null||$tenant_id!=''){
         if($order_id!=null||$order_id!=''){
             $array['order_status']=3;
+            $array['order_datetime3']=date('Y-m-d H:m:s',time());
             $updateStatement = $database->update($array)
                 ->table('orders')
                 ->where('tenant_id','=',$tenant_id)
@@ -602,6 +603,8 @@ $app->put('/alterOrder10',function()use($app){
     if($tenant_id!=null||$tenant_id!=''){
         if($order_id!=null||$order_id!=''){
             $array['order_status']=3;
+            $array['order_datetime2']=date('Y-m-d H:m:s',time());
+            $array['order_datetime3']=date('Y-m-d H:m:s',time());
             $updateStatement = $database->update($array)
                 ->table('orders')
                 ->where('tenant_id','=',$tenant_id)
