29d76881f54d90e682072b6c9ea0257456e3c5b8
GHOSTDO <2121066061@qq.com>
2018-04-18 10:32:50 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- delivery_nedb.php
+++ delivery_nedb.php
@@ -108,6 +108,7 @@ $app->post('/addDelivery',function()use($app) {
             if($lorry_id!=null||$lorry_id!=''){
                 if($delivery_cost!=null||$delivery_cost!=''){
                     $array['tenant_id']=$tenant_id;
+                    date_default_timezone_set("PRC");
                     $array['delivery_datetime']=date('Y-m-d H:i:s',time());
                     $array['exist']=0;
                     $insertStatement = $database->insert(array_keys($array))
