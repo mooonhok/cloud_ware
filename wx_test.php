d9d6e40876cec4feced99c0684ad948408a39e13
GHOSTDO <2121066061@qq.com>
2018-05-02 15:51:15 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- wx_test.php
+++ wx_test.php
@@ -28,42 +28,7 @@ $app->get('/tenant',function()use($app){
 });
 
 
-$app->get('/sontenants',function()use($app){
-    $app->response->headers->set('Access-Control-Allow-Origin','*');
-    $app->response->headers->set('Content-Type','application/json');
-    $tenant_id=$app->request->get('tenant-id');
-    $database=localhost();
-    $selectStatement = $database->select()
-        ->from('tenant')
-        ->where('tenant_id','=',$tenant_id);
-    $stmt = $selectStatement->execute();
-    $data1 = $stmt->fetch();
-    if($data1['business_l']!=null||$data1['business_l']!=""){
-        $selectStatement = $database->select()
-            ->from('tenant')
-            ->where('business_l','=',$data1['business_l']);
-        $stmt = $selectStatement->execute();
-        $data2 = $stmt->fetchAll();
-        for($x=0;$x<count($data2);$x++){
-            $selectStatement = $database->select()
-                ->from('customer')
-                ->where('customer_id','=',$data2[$x]['contact_id'])
-                ->where('tenant_id','=',$data2[$x]['tenant_id']);
-            $stmt = $selectStatement->execute();
-            $data3 = $stmt->fetch();
-            $data2[$x]['telephone']=$data3['customer_name'].$data3['customer_phone'];
-            $selectStatement = $database->select()
-                ->from('wxtprice')
-                ->where('tenant_id','=',$data2[$x]['tenant_id']);
-            $stmt = $selectStatement->execute();
-            $data4 = $stmt->fetchAll();
-            $data2[$x]['routeprice']=$data4;
-        }
-        echo json_encode(array('result'=>'0','desc'=>'','tenants'=>$data2));
-    }else{
-        echo json_encode(array('result'=>'1','desc'=>'尚未有数据','tenant'=>''));
-    }
-});
+
 
 
 $app->run();
