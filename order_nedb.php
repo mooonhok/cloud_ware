b070497c76af45e10813b10c1962bd208c7aeb10
mooonhok <1026413232@qq.com>
2018-05-02 17:14:29 +0800

    修改

--- order_nedb.php
+++ order_nedb.php
@@ -814,6 +814,30 @@ $app->put('/alterOrder16',function()use($app){
     }
 });
 
+$app->put('/alterOrder17',function()use($app){
+    $app->response->headers->set('Content-Type', 'application/json');
+    $tenant_id = $app->request->headers->get("tenant-id");
+    $database = localhost();
+    $body = $app->request->getBody();
+    $body = json_decode($body);
+    $order_id = $body->order_id;
+    $is_sign= $body->is_sign;
+    if($tenant_id!=null||$tenant_id!=''){
+        if($order_id!=null||$order_id!=''){
+            $updateStatement = $database->update(array('is_sign'=>$is_sign))
+                ->table('orders')
+                ->where('tenant_id','=',$tenant_id)
+                ->where('exist','=',0)
+                ->where('order_id','=',$order_id);
+            $affectedRows = $updateStatement->execute();
+            echo json_encode(array("result" => "0", "desc" => "success"));
+        }else{
+            echo json_encode(array("result" => "1", "desc" => "缺少运单id"));
+        }
+    }else{
+        echo json_encode(array("result" => "2", "desc" => "缺少租户id"));
+    }
+});
 
 //根据运单id和发货人名模糊查询
 $app->get('/getOrders_orderid_or_sender', function () use ($app) {
