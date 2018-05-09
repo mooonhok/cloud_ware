c073f8ea0eaa7a488741875943c6d17fa5af61da
GHOSTDO <2121066061@qq.com>
2018-05-09 10:32:54 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- mini.php
+++ mini.php
@@ -12,33 +12,7 @@ use Slim\PDO\Database;
 
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
-$app->post('/getcityname',function()use($app){
-    $app->response->headers->set('Access-Control-Allow-Origin','*');
-    $app->response->headers->set('Content-Type','application/json');
-    $database=localhost();
-    $body=$app->request->getBody();
-    $body=json_decode($body);
-    $long=$body->long;
-    $lat=$body->lat;
-    if($long!=null||$long!=""){
-         if($lat!=null||$lat!=""){
-             $x = (double)$long ;
-             $y = (double)$lat;
-             $x_pi = 3.14159265358979324*3000/180;
-             $z = sqrt($x * $x+$y * $y) + 0.00002 * sin($y * $x_pi);
 
-             $theta = atan2($y,$x) + 0.000003 * cos($x*$x_pi);
-
-             $gb = number_format($z * cos($theta) + 0.0065,6);
-             $ga = number_format($z * sin($theta) + 0.006,6);
-             echo json_encode(array("result"=>"0","desc"=>$ga.','.$gb));
-         }else{
-             echo json_encode(array("result"=>"1","desc"=>"缺少坐标"));
-         }
-    }else{
-        echo json_encode(array("result"=>"1","desc"=>"缺少坐标"));
-    }
-});
 
 
 
