df5b6bd459d360fc3c6b3d03fbcbab4ddeab311f
mooonhok <1026413232@qq.com>
2018-05-10 16:27:20 +0800

    修改

--- background/javascript/pages/order.js
+++ background/javascript/pages/order.js
@@ -83,10 +83,7 @@ function loadorders(order_id,page,company) {
                                 +item.sender.customer_name+'</td><td>'
                                 +item.receiver.customer_name+'</td><td>'
                                 +item.order_datetime1+'</td><td>'
-                                +info+'</td><td id="d'+item.order_id + '"><span style="color:blue; cursor:pointer;">查看</span></td></tr>');
-                                $("#d"+item.order_id).on('click',function(){
-                                    alert(1);
-                                });
+                                +info+'</td><td onclick="order_xq(1'+item.order_id+')"><span style="color:blue; cursor:pointer;">查看</span></td></tr>');
                             });
                             return arr.join('');
                         }();
