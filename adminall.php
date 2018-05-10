3d3e9f1c707346bf40a72f1276befd1766c99f5b
mooonhok <1026413232@qq.com>
2018-05-10 11:49:19 +0800

    修改

--- adminall.php
+++ adminall.php
@@ -1620,9 +1620,9 @@ $app->get('/get_tenant_son',function()use($app){
     $array=array();
     for($i=0;$i<count($data);$i++){
         if($array){
-            array_push($array,$array[$i]['tenant_id']);
+            array_merge($array,$array[$i]['tenant_id']);
         }else{
-            $array=$array[$i]['tenant_id'];
+            $array[0]=$array[$i]['tenant_id'];
         }
     }
     $selectStatement = $database->select()
