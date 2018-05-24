e7a4d1121355e9341540f1a30be543e7dbb16969
GHOSTDO <2121066061@qq.com>
2018-05-24 11:01:03 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- tenant_nedb.php
+++ tenant_nedb.php
@@ -56,6 +56,50 @@ $app->get('/getTenant1',function()use($app){
 
 });
 
+$app->get('/gwgetTenant1',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $database=localhost();
+    $tenant_id=$app->request->get('tenant_id');
+    if($tenant_id!=null||$tenant_id!=''){
+        $selectStatement = $database->select()
+            ->from('tenant')
+            ->where('exist',"=",0)
+            ->where('tenant_id',"=",$tenant_id);
+        $stmt = $selectStatement->execute();
+        $data = $stmt->fetch();
+        if($data!=null){
+            $selectStatement = $database->select()
+                ->from('tenant')
+                ->leftJoin('customer','customer.customer_id','=','tenant.contact_id')
+                ->where('tenant.tenant_id','=',$tenant_id)
+                ->where('customer.tenant_id','=',$tenant_id)
+                ->where('tenant.exist',"=",0);
+            $stmt = $selectStatement->execute();
+            $data1= $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('city')
+                ->where('id', '=', $data1['customer_city_id']);
+            $stmt = $selectStatement->execute();
+            $data2 = $stmt->fetch();
+            $selectStatement = $database->select()
+                ->from('province')
+                ->where('id', '=', $data2['pid']);
+            $stmt = $selectStatement->execute();
+            $data3 = $stmt->fetch();
+            $data1['city']=$data2;
+            $data1['province']=$data3;
+            echo  json_encode(array("result"=>"0","desc"=>"success","tenant"=>$data1));
+        }else{
+            echo  json_encode(array("result"=>"1","desc"=>"租户不存在",));
+        }
+    }else{
+        echo  json_encode(array("result"=>"2","desc"=>"租户id不存在"));
+    }
+
+});
+
+
 $app->put('/alterTenant1',function()use($app){
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
