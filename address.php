d6cc696e709a8b15df0b39b70f536f393957a915
GHOSTDO <2121066061@qq.com>
2018-06-12 18:00:06 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- address.php
+++ address.php
@@ -20,16 +20,35 @@ $app->post('/addAddress',function()use($app){
     $body=$app->request->getBody();
     $body=json_decode($body);
     $tenant_id=$body->tenant_id;
+    $name=$body->name;
+    $phone=$body->phone;
+    $province_id=$body->province_id;
+    $city_id=$body->city_id;
+    $address=$body->address;
     $array=array();
     foreach($body as $key=>$value){
         $array[$key]=$value;
     }
     if($tenant_id!=null||$tenant_id!=""){
+        $selectStatement = $database->select()
+            ->from('address')
+            ->where('tenant_id','=',$tenant_id)
+            ->where('phone','=',$phone)
+            ->where('address','=',$address)
+            ->where('city_id','=',$city_id)
+            ->where('province_id','=',$province_id)
+            ->where('name','=',$name);
+        $stmt = $selectStatement->execute();
+        $data = $stmt->fetch();
+        if($data==null){
         $insertStatement = $database->insert(array_keys($array))
             ->into('address')
             ->values(array_values($array));
         $insertId = $insertStatement->execute(false);
         echo json_encode(array("result" => "0", "desc" => "success"));
+        }else{
+            echo json_encode(array("result" => "2", "desc" => "该记录已经存在"));
+        }
     }else{
         echo json_encode(array("result" => "1", "desc" => "缺少租户id"));
     }
@@ -50,9 +69,9 @@ $app->get('/limitAddresses',function()use($app){
             ->limit((int)$per_page,(int)$per_page*(int)$page);
             $stmt = $selectStatement->execute();
            $data = $stmt->fetchAll();
-        echo json_encode(array("result"=>"0","desc"=>"success","address"=>$data));
+        echo json_encode(array("result"=>"0","desc"=>"success","addresses"=>$data));
     }else{
-        echo json_encode(array("result"=>"2","desc"=>"缺少租户id","address"=>""));
+        echo json_encode(array("result"=>"2","desc"=>"缺少租户id"));
     }
 });
 
@@ -67,9 +86,9 @@ $app->get('/getAddresses',function()use($app){
             ->where('tenant_id','=',$tenant_id);
         $stmt = $selectStatement->execute();
         $data = $stmt->fetchAll();
-        echo json_encode(array("result"=>"0","desc"=>"success","address"=>$data));
+        echo json_encode(array("result"=>"0","desc"=>"success","addresses"=>$data));
     }else{
-        echo json_encode(array("result"=>"2","desc"=>"缺少租户id","address"=>""));
+        echo json_encode(array("result"=>"2","desc"=>"缺少租户id"));
     }
 });
 
