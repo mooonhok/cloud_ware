7b09f256830daaa8feae83a2714a00bdc4643e7d
mooonhok <1026413232@qq.com>
2018-06-21 20:26:12 +0800

    修改

--- app.php
+++ app.php
@@ -647,7 +647,7 @@ $app->post('/persoonmessage',function()use($app){
         $selectStament=$database->select()
             ->from('app_lorry')
             ->where('exist','=',0)
-            ->where('is_bind','=',0)
+//            ->where('is_bind','=',0)
             ->where('app_lorry_id','=',$lorryid);
         $stmt=$selectStament->execute();
         $data1=$stmt->fetch();
@@ -924,6 +924,7 @@ $app->get('/wait_schedulings',function()use($app){
                 ->from('lorry')
                 ->where('exist','=',0)
                 ->where('tenant_id','!=','0')
+                ->where('plate_number','=',$data1['plate_number'])
                 ->where('driver_phone','=',$data1['phone']);
             $stmt=$selectStament->execute();
             $data2=$stmt->fetchAll();
@@ -1006,6 +1007,7 @@ $app->get('/schistory',function()use($app){
                 ->from('lorry')
                 ->where('exist','=',0)
                 ->where('tenant_id','!=','0')
+                ->where('plate_number','=',$data1['plate_number'])
                 ->where('driver_phone','=',$data1['phone']);
             $stmt=$selectStament->execute();
             $data2=$stmt->fetchAll();
@@ -1086,6 +1088,7 @@ $app->get('/scnoaccept',function()use($app){
                 ->from('lorry')
                 ->where('exist','=',0)
                 ->where('tenant_id','!=','0')
+                ->where('plate_number','=',$data1['plate_number'])
                 ->where('driver_phone','=',$data1['phone']);
             $stmt=$selectStament->execute();
             $data2=$stmt->fetchAll();
@@ -2956,6 +2959,257 @@ $app->post('/changeIs_bind',function()use($app){
         echo json_encode(array('result'=>'0','desc'=>'success'));
     }else{
       echo json_encode(array('result'=>'1','desc'=>'缺少司机id'));
+    }
+});
+
+$app->get('/getAppLorry',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $phone= $app->request->get("phone");
+    $database=localhost();
+    if($phone!=null||$phone!=""){
+        $selectStament=$database->select()
+            ->from('app_lorry')
+            ->where('exist','=',0)
+            ->where('phone','=',$phone);
+        $stmt=$selectStament->execute();
+        $data1=$stmt->fetchAll();
+        echo json_encode(array('result' => '0', 'desc' => 'success','app_lorrys'=>$data1));
+    }else{
+        echo json_encode(array('result' => '1', 'desc' => '缺少司机电话'));
+    }
+});
+
+$app->post('/changeIsBind',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $body = $app->request->getBody();
+    $body=json_decode($body);
+    $lorry_id_o=$body->lorry_id_o;
+    $lorry_id=$body->lorry_id;
+    $phone=$body->phone;
+    $database=localhost();
+    if($lorry_id!=''||$lorry_id!=null){
+        $updateStatement = $database->update(array('is_bind'=>1))
+            ->table('app_lorry')
+            ->where('exist','=',0)
+            ->where('app_lorry_id','=',$lorry_id_o);
+        $affectedRows = $updateStatement->execute();
+        $updateStatement = $database->update(array('is_bind'=>0))
+            ->table('app_lorry')
+            ->where('exist','=',0)
+            ->where('app_lorry_id','=',$lorry_id);
+        $affectedRows = $updateStatement->execute();
+        echo json_encode(array('result'=>'0','desc'=>'success'));
+    }else{
+        echo json_encode(array('result'=>'1','desc'=>'缺少司机id'));
+    }
+});
+
+$app->post('/changeIsExist',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $body = $app->request->getBody();
+    $body=json_decode($body);
+    $lorry_id=$body->lorry_id;
+    $num=0;
+    $database=localhost();
+        if($lorry_id!=null||$lorry_id!=""){
+            $selectStament=$database->select()
+                ->from('app_lorry')
+                ->where('exist','=',0)
+//                ->where('is_bind','=',0)
+                ->where('app_lorry_id','=',$lorry_id);
+            $stmt=$selectStament->execute();
+            $data1=$stmt->fetch();
+            if($data1!=null){
+                $selectStament=$database->select()
+                    ->from('lorry')
+                    ->where('exist','=',0)
+                    ->where('flag','=',0)
+                    ->where('tenant_id','!=',0)
+                    ->where('plate_number','=',$data1['plate_number'])
+                    ->where('driver_phone','=',$data1['phone']);
+                $stmt=$selectStament->execute();
+                $data2=$stmt->fetchAll();
+                if($data2!=null){
+                    for($i=0;$i<count($data2);$i++){
+                        $selectStament=$database->select()
+                            ->from('scheduling')
+                            ->where('exist','=',0)
+                            ->where('scheduling_status','!=',1)
+                            ->where('scheduling_status','!=',5)
+                            ->where('scheduling_status','!=',7)
+                            ->where('scheduling_status','!=',8)
+                            ->where('scheduling_status','!=',9)
+                            ->where('tenant_id','=',$data2[$i]['tenant_id'])
+                            ->where('lorry_id','=',$data2[$i]['lorry_id']);
+                        $stmt=$selectStament->execute();
+                        $data3=$stmt->fetchAll();
+                        if($data3!=null){
+                            $num+=count($data3);
+                        }
+                    }
+                }
+                if($num==0){
+                    $updateStatement = $database->update(array('exist'=>2))
+                        ->table('app_lorry')
+                        ->where('exist','=',0)
+                        ->where('app_lorry_id','=',$lorry_id);
+                    $affectedRows = $updateStatement->execute();
+                    echo json_encode(array('result'=>'0','desc'=>'success'));
+                }else{
+                    echo json_encode(array('result'=>'4','desc'=>'您还有清单未处理'));
+                }
+            }else{
+                echo json_encode(array('result' => '2', 'desc' => '司机不存在'));
+            }
+        }else{
+            echo json_encode(array('result'=>'1','desc'=>'缺少司机id'));
+        }
+});
+
+$app->post('/addlorryA',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $database=localhost();
+    $body=$app->request->getBody();
+    $body=json_decode($body);
+    $long=$body->long;
+    $ctype=$body->ctype;
+    $cweight=$body->cweight;
+    $lorryid=$body->lorryid;
+    $plate_number=$body->plate_number;
+    if($lorryid!=null||$lorryid!="") {
+            if ($long != null || $long != "") {
+                if ($ctype != null || $ctype != "") {
+                    if($plate_number!=null||$plate_number!=""){
+
+
+                        $selectStament=$database->select()
+                            ->from('app_lorry')
+                            ->orderBy('id','desc')
+                            ->limit(1);
+                        $stmt=$selectStament->execute();
+                        $datab=$stmt->fetch();
+                        $selectStament = $database->select()
+                            ->from('app_lorry')
+                            ->where('app_lorry_id', '=', $lorryid);
+                        $stmt = $selectStament->execute();
+                        $dataa = $stmt->fetch();
+                        $selectStament=$database->select()
+                            ->from('app_lorry')
+                            ->where('exist', '=', 0)
+                            ->where('plate_number', '=', $plate_number)
+                            ->where('phone', '=', $dataa['phone']);
+                        $stmt=$selectStament->execute();
+                        $datac=$stmt->fetch();
+                        if(!$datac){
+                            $insertStatement = $database->insert(array('app_lorry_id','phone','password','id_number','name','driver_license_fp','driver_license_tp','identity_card_z','identity_card_f','is_bind','exist'))
+                                ->into('app_lorry')
+                                ->values(array($datab['id']+1,$dataa['phone'],$dataa['password'],$dataa['id_number'],$dataa['name'],$dataa['driver_license_fp'],$dataa['driver_license_tp'],$dataa['identity_card_z'],$dataa['identity_card_f'],1,1));
+                            $insertId = $insertStatement->execute(false);
+                            if ($dataa != null) {
+                                $arrays['flag'] = 0;
+                                $arrays['type'] = $ctype;
+                                $arrays['length'] = $long;
+                                $arrays['plate_number']=$plate_number;
+                                $arrays['deadweight']=$cweight;
+                                $updateStatement = $database->update($arrays)
+                                    ->table('app_lorry')
+                                    ->where('app_lorry_id', '=', ($datab['id']+1));
+                                $affectedRows = $updateStatement->execute();
+                                echo json_encode(array('result' => '0', 'desc' => '', 'lorryid' => $lorryid));
+                            } else {
+                                echo json_encode(array('result' => '4', 'desc' => '未填写电话号码'));
+                            }
+                        }else{
+                            echo json_encode(array('result' => '6', 'desc' => '该车辆已注册'));
+                        }
+                    }else{
+                        echo json_encode(array('result' => '5', 'desc' => '车牌号不能为空'));
+                    }
+                } else {
+                    echo json_encode(array('result' => '3', 'desc' => '未选择车辆类型'));
+                }
+            } else {
+                echo json_encode(array('result' => '2', 'desc' => '未填写车辆长度'));
+            }
+    }else{
+        echo json_encode(array('result' => '5', 'desc' => '未填写电话号码'));
+    }
+});
+
+$app->post('/addlorryB',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $database=localhost();
+    $file_url=file_url();
+    $body=$app->request->getBody();
+    $body=json_decode($body);
+    $lorryid=$body->lorryid;
+    $id_pic5=$body->id_pic5;
+    $id_pic6=$body->id_pic6;
+    $lujing5=null;
+    $lujing6=null;
+    if($id_pic5!=null){
+        $base64_image_content = $id_pic5;
+//匹配出图片的格式
+        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)) {
+            $type = $result[2];
+            date_default_timezone_set("PRC");
+            $time1 = time();
+            $new_file = "/files/lorry3/" . date('Ymd', $time1) . "/";
+            if (!file_exists($new_file)) {
+//检查是否有该文件夹，如果没有就创建，并给予最高权限
+                mkdir($new_file, 0700);
+            }
+            $new_file = $new_file . $time1 . ".{$type}";
+            if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))) {
+                $lujing5 = $file_url."lorry3/" . date('Ymd', $time1) . "/" . $time1 . ".{$type}";
+            }
+        }
+        $arrays['identity_card_z']=$lujing5;
+        if($id_pic6!=null){
+            $base64_image_content = $id_pic6;
+//匹配出图片的格式
+            if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)) {
+                $type = $result[2];
+                date_default_timezone_set("PRC");
+                $time1 = time();
+                $new_file = "/files/lorry4/" . date('Ymd', $time1) . "/";
+                if (!file_exists($new_file)) {
+//检查是否有该文件夹，如果没有就创建，并给予最高权限
+                    mkdir($new_file, 0700);
+                }
+                $new_file = $new_file . $time1 . ".{$type}";
+                if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))) {
+                    $lujing6 = $file_url."lorry4/" . date('Ymd', $time1) . "/" . $time1 . ".{$type}";
+                }
+            }
+            $arrays['identity_card_f']=$lujing6;
+            $arrays['flag']=0;
+            $selectStament=$database->select()
+                ->from('app_lorry')
+                ->where('app_lorry_id','=',$lorryid);
+            $stmt=$selectStament->execute();
+            $data1=$stmt->fetch();
+            if($data1!=null){
+                $arrays['exist']=0;
+                $arrays['is_bind']=1;
+                $updateStatement = $database->update($arrays)
+                    ->table('app_lorry')
+                    ->where('app_lorry_id','=',$lorryid);
+                $affectedRows = $updateStatement->execute();
+                echo json_encode(array('result'=>'0','desc'=>'success'));
+            }else {
+                echo json_encode(array('result' => '4', 'desc' => '车辆信息有误'));
+            }
+        }else{
+            echo json_encode(array('result' => '2', 'desc' => '没有行驶证反面图片'));
+        }
+    }else{
+        echo json_encode(array('result' => '1', 'desc' => '没有行驶证正面图片'));
     }
 });
 
