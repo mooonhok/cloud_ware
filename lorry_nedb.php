6d45c83887bc825b6d7d73cfaf2cbf481ef0c668
GHOSTDO <2121066061@qq.com>
2018-06-25 14:18:22 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- lorry_nedb.php
+++ lorry_nedb.php
@@ -545,8 +545,10 @@ $app->put('/alterLorry',function()use($app){
     $app->response->headers->set('Content-Type', 'application/json');
     $tenant_id = $app->request->headers->get("tenant-id");
     $database = localhost();
-    $lorry_id= $app->request->get('lorry_id');
-    $exist= $app->request->get('exist');
+    $body=$app->request->getBody();
+    $body=json_decode($body);
+    $lorry_id= $body->lorry_id;
+    $exist= $body->exist;
     if($tenant_id!=null||$tenant_id!=''){
         $updateStatement = $database->update(array('exist'=>$exist))
             ->table('lorry')
