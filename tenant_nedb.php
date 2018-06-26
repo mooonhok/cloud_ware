ddcf6988258f810c778966cbcd11aa1daa96895a
GHOSTDO <2121066061@qq.com>
2018-06-26 18:02:26 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- tenant_nedb.php
+++ tenant_nedb.php
@@ -100,25 +100,24 @@ $app->get('/gwgetTenant1',function()use($app){
 });
 
 
-$app->put('/alterTenant1',function()use($app){
+$app->get('/getTenant0',function()use($app){
     $app->response->headers->set('Access-Control-Allow-Origin','*');
     $app->response->headers->set('Content-Type','application/json');
-    $tenant_id = $app->request->headers->get("tenant-id");
-    $database=localhost();
-    $body=$app->request->getBody();
-    $body=json_decode($body);
-    $is_join=$body->is_join;
-    if($tenant_id!=null||$tenant_id!=''){
-        $updateStatement = $database->update(array('is_join'=>$is_join))
-            ->table('tenant')
-            ->where('exist','=',0)
-            ->where('tenant_id','=',$tenant_id);
-        $affectedRows = $updateStatement->execute();
-        echo  json_encode(array("result"=>"0","desc"=>"sucess"));
-    }else{
-        echo  json_encode(array("result"=>"1","desc"=>"租户id不存在"));
-    }
+    $database = localhost();
+    $tenant_id=$app->request->get('tenant_id');
+    $company=$app->request->get('company');
+    $tenant_num=$app->request->get('tenant_num');
+    $selectStatement = $database->select()
+        ->from('tenant')
+        ->where('company','=',$company)
+        ->where('tenant_num','=',$tenant_num)
+        ->where('tenant_id','=',$tenant_id);
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetch();
+    echo  json_encode(array("result"=>"0","desc"=>"success","tenant"=>$data));
 });
+
+
 
 $app->run();
 
