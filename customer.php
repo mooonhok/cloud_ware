66f3307d3dfc652d3ad5467676cf70a8dc6c7b33
GHOSTDO <2121066061@qq.com>
2018-06-26 13:28:46 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- customer.php
+++ customer.php
@@ -953,9 +953,10 @@ function array_unset_tt($arr,$key){
 
 function array_unique_fb($array2D){
     foreach ($array2D as $k=>$v){
-        if($k!="customer_id"){}
+        if($k!="customer_id"){
         $v=join(',',$v); //降维,也可以用implode,将一维数组转换为用逗号连接的字符串
         $temp[$k]=$v;
+        }
     }
     $temp=array_unique($temp); //去掉重复的字符串,也就是重复的一维数组
     foreach ($temp as $k => $v){
