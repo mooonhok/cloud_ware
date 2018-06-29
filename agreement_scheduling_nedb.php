374752cc7c2d09c6aaa3ed8db992897397f945a6
GHOSTDO <2121066061@qq.com>
2018-06-29 22:10:11 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- agreement_scheduling_nedb.php
+++ agreement_scheduling_nedb.php
@@ -237,6 +237,82 @@ $app->get('/limitAgreementSchedulings',function()use($app) {
     }
 });
 
+
+
+$app->post('/addAgreementScheduling',function()use($app) {
+    $app->response->headers->set('Content-Type', 'application/json');
+    $database = localhost();
+    $tenant_id = $app->request->headers->get("tenant-id");
+    $body = $app->request->getBody();
+    $body = json_decode($body);
+    $secondparty_id=$body->secondparty_id;
+    $pay_method=$body->pay_method;
+    $deadline=$body->deadline;
+    $agreement_require=$body->agreement_require;
+    $rcity=$body->rcity;
+    $is_ticket=$body->is_ticket;
+    $company_id=$body->company_id;
+    $scheduling_ary=$body->scheduling_ary;
+    $freight=$body->freight;
+    $tenant_num=$body->tenant_num;
+    $array1=null;
+    foreach ($scheduling_ary as $key => $value) {
+        $array1[$key] = $value;
+    }
+    if($tenant_id!=null||$tenant_id!=''){
+        $selectStatement = $database->select()
+            ->from('agreement')
+            ->where('tenant_id', '=', $tenant_id);
+        $stmt = $selectStatement->execute();
+        $data9 = $stmt->fetchAll();
+        $agreement_id=null;
+        if((count($data9)+1)<10){
+            $agreement_id='HT'.$tenant_num."00000".(count($data9)+1);
+        }else if((count($data9)+1)<100&&(count($data9)+1)>9){
+            $agreement_id='HT'.$tenant_num."0000".(count($data9)+1);
+        }else if((count($data9)+1)<1000&&(count($data9)+1)>99){
+            $agreement_id='HT'.$tenant_num."000".(count($data9)+1);
+        }else if((count($data9)+1)<10000&&(count($data9)+1)>999){
+            $agreement_id='HT'.$tenant_num."00".(count($data9)+1);
+        }else if((count($data9)+1)<100000&&(count($data9)+1)>9999){
+            $agreement_id='HT'.$tenant_num."0".(count($data9)+1);
+        }else if((count($data9)+1)<1000000&&(count($data9)+1)>99999){
+            $agreement_id='HT'.$tenant_num.(count($data9)+1);
+        }
+        for($x=0;$x<count($array1);$x++){
+            $selectStatement = $database->select()
+                ->from('agreement')
+                ->where('tenant_id', '=', $tenant_id)
+                ->where('agreement_id','=',$agreement_id)
+                ->where('scheduling_id','=',$array1[$x])
+                ->where("exist",'=',0);
+            $stmt = $selectStatement->execute();
+            $data = $stmt->fetch();
+            $updateStatement = $database->update(array('is_contract'=>2))
+                ->table('scheduling')
+                ->where('scheduling_id','=',$array1[$x])
+                ->where('tenant_id','=',$tenant_id);
+            $affectedRows = $updateStatement->execute();
+            if($data==null) {
+                $insertStatement = $database->insert(array("tenant_id", "agreement_id", "scheduling_id", "exist"))
+                    ->into('agreement_schedule')
+                    ->values(array($tenant_id, $agreement_id, $array1[$x], 0));
+                $insertId = $insertStatement->execute(false);
+            }
+        }
+        $insertStatement = $database->insert(array("agreement_id","secondparty_id","pay_method","deadline","agreement_require","rcity","is_ticket","company_id","freight"))
+            ->into('agreement')
+            ->values(array($agreement_id,$secondparty_id,$pay_method,$deadline,$agreement_require,$rcity,$is_ticket,$company_id,$freight));
+        $insertId = $insertStatement->execute(false);
+        echo json_encode(array("result" => "0", "desc" => "success"));
+    }else{
+        echo json_encode(array("result" => "1", "desc" => "缺少租户id"));
+    }
+});
+
+
+
+
 $app->run();
 
 function file_url(){
