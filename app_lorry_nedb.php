11bd64688be625970a05a5223134c59baa243234
GHOSTDO <2121066061@qq.com>
2018-07-13 10:27:09 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- app_lorry_nedb.php
+++ app_lorry_nedb.php
@@ -131,6 +131,72 @@ $app->get('/getAppLorry_app',function()use($app){
     }
 });
 
+$app->get('/checkAppLorry',function()use($app){
+    $app->response->headers->set('Access-Control-Allow-Origin','*');
+    $app->response->headers->set('Content-Type','application/json');
+    $tenant_id = $app->request->headers->get("tenant-id");
+    $body=$app->request->getBody();
+    $body=json_decode($body);
+    $driver_name = $app->request->get('driver_name');
+    $driver_phone = $app->request->get('driver_phone');
+    $plate_number = $app->request->get('plate_number');
+    $flag=$app->request->get('flag');
+    $database=localhost();
+    if($tenant_id!=''||$tenant_id!=null){
+        $selectStatement = $database->select()
+            ->from('app_lorry')
+            ->where('flag','=',$flag)
+            ->where('exist','=',0)
+            ->where('phone','=',$driver_phone)
+            ->where('plate_number','=',$plate_number)
+            ->where('name','=',$driver_name);
+        $stmt = $selectStatement->execute();
+        $data1= $stmt->fetch();
+        if($data1==null){
+            if($data1['lorry_status']!=1){
+                $selectStatement = $database->select()
+                    ->from('lorry')
+                    ->where('flag','=',$flag)
+                    ->where('tenant_id','=',$tenant_id)
+                    ->where('driver_phone','=',$driver_phone)
+                    ->where('plate_number','=',$plate_number)
+                    ->where('driver_name','=',$driver_name);
+                $stmt = $selectStatement->execute();
+                $data2= $stmt->fetch();
+                if($data2!=null){
+                    if($data2['exist']==0){
+                        echo json_encode(array('result'=>'4','desc'=>'车辆已添加过'));
+                    }else if($data2['exist']==1){
+                        echo json_encode(array('result'=>'3','desc'=>'车辆被加入黑名单'));
+                    }
+                }else{
+                    $selectStatement = $database->select()
+                        ->from('lorry_length')
+                        ->where('lorry_length_id','=',$data1['length']);
+                    $stmt = $selectStatement->execute();
+                    $data5= $stmt->fetch();
+                    $data1['lorry_length_name']=$data5['lorry_length'];
+                    $data1['lorry_load_name']=$data1['deadweight'];
+                    $selectStatement = $database->select()
+                        ->from('lorry_type')
+                        ->where('lorry_type_id','=',$data1['type']);
+                    $stmt = $selectStatement->execute();
+                    $data4= $stmt->fetch();
+                    $data1['lorry_type_name']=$data4['lorry_type_name'];
+                    echo json_encode(array("result"=>"0","desc"=>"","lorrys"=>$data1));
+                }
+            }else{
+                echo json_encode(array('result'=>'2','desc'=>'正在修改资料'));
+            }
+        }else{
+            echo json_encode(array('result'=>'1','desc'=>'未注册交付帮手'));
+        }
+    }else{
+        echo json_encode(array('result'=>'4','desc'=>'缺少租户id'));
+    }
+});
+
+
 $app->run();
 
 function localhost(){
